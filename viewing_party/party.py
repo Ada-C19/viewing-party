caa1837e05ed838937086342d044b8300cf8db78
Janice <janicenguyenn@gmail.com>
2023-03-27 11:59:32 -0700

    completed create_movie function

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,8 +1,15 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    pass
+    new_movie = {}
+    if not title or not genre or not rating:
+        return None
 
+    else:
+        new_movie['title'] = title
+        new_movie['genre'] = genre
+        new_movie['rating'] = rating
+    return new_movie
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
