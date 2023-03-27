935d538f41b69195d9a3ae22b508c8dff83acbc1
Niambi <niambiada19@gmail.com>
2023-03-27 14:27:33 -0700

    defined add_to_watched in party.py & passed test_adds_movie_to_non_empty_user_watched

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -3,11 +3,15 @@
 def create_movie(title, genre, rating):
     if title == None or genre == None or rating == None:
         new_movie = None
-    # elif rating == None:
-    #     new_movie = None
     else:
         new_movie = {"title": title, "genre": genre, "rating": rating}
     return new_movie
+
+def add_to_watched(user_data, movie):
+    watched_list = [movie]
+    user_data["watched"] = watched_list
+    return user_data
+
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
