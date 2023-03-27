cd1a3592a77d133cfc6324f285ffe3a8eb7744bf
Amanda <jonesamanda996@gmail.com>
2023-03-27 15:09:27 -0400

    wave:1 updates

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -12,13 +12,39 @@ def create_movie(title, genre, rating):
 
 
 def add_to_watched(user_data, movie): 
-    if movie:
+    if movie.items():
         user_data["watched"].append(movie)
-        return user_data
-    
+    return user_data
+
+def add_to_watchlist(user_data, movie):
+    if movie:
+        user_data["watchlist"].append(movie)
+    return user_data
+
+def watch_movie(user_data, title): 
+    # if title not in user_data["watchlist"]:
+    #     return user_data
+    for value in user_data.values():
+        for movie in value: 
+            if movie["title"] == title: 
+                user_data["watchlist"].remove(movie)
+                user_data["watched"].append(movie)
+
+    return user_data
+
+
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
+def get_watched_avg_rating(user_data): 
+    sum = 0
+    rating_average = 0
+    for watched in user_data["watched"]: 
+        sum += watched["rating"]
+        rating_average = sum / len(user_data["watched"])
+    return rating_average
+
+
 
 
 # -----------------------------------------
