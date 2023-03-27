4edd3a9d3360c3c5cc57b3044a5c640eb5805ebc
Carolyn Qi <qi.carolyn@gmail.com>
2023-03-27 12:00:53 -0700

    Add Wave 1 Add_To_watched Function

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,5 +1,5 @@
 # ------------- WAVE 1 --------------------
-
+import copy
 def create_movie(title, genre, rating):
     if not title or not genre or not rating:
         return None
@@ -8,6 +8,12 @@ def create_movie(title, genre, rating):
         "genre": genre,
         "rating": rating    }
     return movie    
+
+def add_to_watched(user_data, movie):
+    user_data_copy = copy.deepcopy(user_data)
+    user_data_copy["watched"].append(movie)
+    return user_data_copy
+
 
 
 # -----------------------------------------
