712c6da76376ab2511b34c1dfe6e928c62f15dd6
Ariel Higuera <purpl3m00ns@gmail.com>
2023-03-27 11:58:11 -0700

    create_movie, add_to_watched functions in party.py

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,7 +1,13 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    pass
+    if title is None or genre is None or rating is None:
+        return None
+    return {"title": title, "genre": genre, "rating": rating}
+
+def add_to_watched(user_data, movie):
+    user_data["watched"].append(create_movie(movie["title"], movie["genre"], movie["rating"]))
+    return user_data
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
