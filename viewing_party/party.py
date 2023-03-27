2168192294903d7b3188aa31fb16718810a946ae
Anna Lima <annavanwesep@gmail.com>
2023-03-27 14:41:31 -0700

    added function names for wave1 with pass statements for each

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,7 +1,22 @@
+MOVIE_TITLE_1 = "It Came from the Stack Trace"
+GENRE_1 = "Horror"
+RATING_1 = 3.5
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
+    if not title or genre or rating:
+        return None
+
+def add_to_watched(user_data, movie):
     pass
+
+def add_to_watchlist(user_data, movie):
+    pass
+
+def watch_movie(user_data, title):
+    pass
+
+
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
