61784cde442017337a157b08a32b3515e3563334
Limary Gonzalez <limaryg11@gmail.com>
2023-03-27 12:58:00 -0700

    Wave 01 updated- tests still failing

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -4,19 +4,22 @@ def create_movie(title, genre, rating):
     # if the parameters are truthy; return dictionary
     # 3 keys: title, genre, rating
 
-    if title == False or genre == False or rating == False:
-        return None
-    
     movie_ratings = {}
 
+    if title == False or genre == False or rating == False:
+        return None
+
+    
     movie_ratings["title"] = title
     movie_ratings["genre"] = genre
     movie_ratings["rating"] = rating
 
     return movie_ratings
 
+def add_to_watched(user_data, movie):
+    user_data["watched"] = movie
 
-
+    return user_data
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
