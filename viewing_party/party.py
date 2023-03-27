7b1dad1fadd2ef623ce735bfaa6cdae2d0b45958
Bella <bellaiam@uw.edu>
2023-03-27 11:54:16 -0700

    init

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,7 +1,19 @@
 # ------------- WAVE 1 --------------------
-
+# - take three parameters: `title`, `genre`, `rating`
+# - If those three attributes are truthy, then return a dictionary. This dictionary should...
+#   - Have three key-value pairs, with specific keys
+#   - The three keys should be `"title"`, `"genre"`, and `"rating"`
+#   - The values of these key-value pairs should be appropriate values
+# - If `title` is falsy, `genre` is falsy, or `rating` is falsy, this function should return `None`
 def create_movie(title, genre, rating):
-    pass
+    movies_info = {}
+    movies_info['title'] = title
+    movies_info['genre'] = genre
+    movies_info['rating'] = rating
+
+    if not 'title' or not 'genre' or not 'rating':
+        return None
+    return movies_info
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
