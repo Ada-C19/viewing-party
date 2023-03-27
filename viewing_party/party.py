14e13cc2d335e5499e12b35c028b0a3a34eb0e53
Julie Loh <jlohusky@gmail.com>
2023-03-27 15:50:06 -0700

    finished wave_01, 02, and started get_unique_watched() together

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,17 +1,128 @@
+import pytest
+from viewing_party.party import *
+from tests.test_constants import *
+
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    pass
+    movie_dict = {}
 
+    if title and genre and rating:
+        movie_dict["title"] = title
+        movie_dict["genre"] = genre
+        movie_dict["rating"] = rating
+    else:
+        return None
+
+    return movie_dict
+
+
+def add_to_watched(user_data , movie):
+    user_data["watched"].append(movie)
+
+    return user_data
+
+def add_to_watchlist(user_data, movie):
+    user_data["watchlist"].append(movie)
+
+    return user_data
+
+
+def watch_movie(user_data, title):
+    # for dict in user_data["watchlist"]:
+    #     if title in dict["title"]:
+    #         removed_movie = user_data["watchlist"].pop(0)
+    for i in range(0,len(user_data["watchlist"])):
+        if title in user_data["watchlist"][i]["title"]:
+            removed_movie = user_data["watchlist"].pop(i)
+            user_data["watched"].append(removed_movie)
+
+    return user_data
+
+        
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
+def get_watched_avg_rating(user_data):
+    sum_rating = 0.0
+    for dict in user_data["watched"]:
+        sum_rating += dict["rating"]
+    
+    try:
+        avg_rating = sum_rating / len(user_data["watched"])
+    except ZeroDivisionError:
+        return 0.0
 
+    return avg_rating
+
+
+def get_most_watched_genre(user_data):
+    genre_dict = {}
+
+    if len(user_data["watched"]) == 0:
+        return None
+    
+    for dict in user_data["watched"]:
+        if not dict["genre"] in genre_dict:
+            genre_dict[dict["genre"]] = 1
+
+        elif dict["genre"] in genre_dict:
+            genre_dict[dict["genre"]] += 1
+    
+    most_watched = ["", 0]
+
+    for key,value in genre_dict.items():
+        if value > most_watched[1]:
+            most_watched[0] = key
+            most_watched[1] = value
+
+    return most_watched[0]
 
 # -----------------------------------------
 # ------------- WAVE 3 --------------------
 # -----------------------------------------
 
+def get_unique_watched(user_data):
+    user_list = []
+    friends_list = []
+    final_list = []
+
+    for dict in user_data["watched"]:
+        print(dict["title"])
+        user_list.append(dict["title"])
+        
+    for movie_dict in user_data["friends"]:
+        # list of dictionaries in friends
+        for i in range(len(user_data["friends"])):
+            # list of dictionaries in watched
+            for movie_dict in user_data["friends"][i]["watched"]:
+                print(movie_dict["title"])
+                friends_list.append(movie_dict["title"])
+
+    user_set = set(user_list)
+    friends_set = set(friends_list)
+
+    print(f"user_set is {user_set}")
+    print(f"friends_set is {friends_set}")
+    #unique_movies = user_set.difference(friends_set)
+    unique_movies = user_set - friends_set
+    print(f"unique_movies is {unique_movies}")
+    unique_movies_list = list(unique_movies)
+    print(f"unique_movies_list is {unique_movies_list}")
+
+    for movie_dict in user_data["watched"]:
+        for unique_movie in unique_movies_list:
+            if unique_movie in movie_dict["title"]:
+                final_list.append(movie_dict)
+            
+    return final_list
+
+amandas_data = clean_wave_3_data()
+print(amandas_data)
+amandas_unique_movies = get_unique_watched(amandas_data)
+# print(amandas_unique_movies)
+    
+        
         
 # -----------------------------------------
 # ------------- WAVE 4 --------------------
