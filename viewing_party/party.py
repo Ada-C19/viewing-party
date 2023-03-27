b3031330bf752a45ae72c644cea932b00b4a4116
Rainacam <rainacam12@gmail.com>
2023-03-27 12:06:25 -0400

    wrote function create movie

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,10 +1,27 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    pass
+    movies = {}
+    
+    # If those three attributes are truthy, then return a dictionary. This dictionary should...
+    if title and genre and rating:
+        # movies.update(new_movies)
+        return {
+        "title": title,
+        "genre": genre,
+        "rating": rating
+    }
+    else:
+    # - If `title` is falsy, `genre` is falsy, or `rating` is falsy, this function should return `None`
+        return None
 
-# test comment 
-# test comment # 2
+
+print(create_movie("title", "genre", "rating"))
+
+
+#   - Have three key-value pairs, with specific keys
+#   - The three keys should be `"title"`, `"genre"`, and `"rating"`
+#   - The values of these key-value pairs should be appropriate values
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
