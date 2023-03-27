f3dfccda167f72b6201d27ea108d6ae2b3f1b101
Madison Jackson <madi.jackson29@gmail.com>
2023-03-27 14:46:01 -0700

    worked through wave1 problems 1-8!

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,7 +1,28 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    pass
+    new_movie = {}
+    new_movie["title"] = title
+    new_movie["genre"] = genre
+    new_movie["rating"] = rating
+    for key, value in new_movie.items():
+        if value == None:
+            return None
+
+    return new_movie
+
+def add_to_watched(user_data, movie):
+    user_data["watched"].append(movie)
+    add_movie = user_data
+    return add_movie
+
+def add_to_watchlist(user_data, movie):
+    user_data["watchlist"].append(movie)
+    watch_list = user_data
+    return watch_list
+
+# def watch_movie():
+
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
