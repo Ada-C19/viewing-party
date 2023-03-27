830f2e5779e42ead5e6f3443c35a9e29e13df883
Allie Soliz <solizale@gmail.com>
2023-03-27 15:01:05 -0400

    creates create_movie() function

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,6 +1,28 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
+    # check if title, genre, or rating is falsy
+    if (not title) or (not genre) or (not rating):
+        # return None
+        return None
+    # check if title_value and genre_value are string, rating is a float
+    if (not isinstance(title, str)) or (not isinstance(genre, str)) or (not isinstance(rating, float)):
+        # raise ValueError("Invalid input")
+        return None
+    # initialize empty dictionary
+    new_movie = {}
+
+    new_movie["title"] = title
+    new_movie["genre"] = genre
+    new_movie["rating"] = float(rating)
+
+    return new_movie
+
+
+create_movie("Scream 6", "Horror", 4.0)
+
+
+def add_to_watched():
     pass
 
 # -----------------------------------------
@@ -12,7 +34,7 @@ def create_movie(title, genre, rating):
 # ------------- WAVE 3 --------------------
 # -----------------------------------------
 
-        
+
 # -----------------------------------------
 # ------------- WAVE 4 --------------------
 # -----------------------------------------
@@ -20,4 +42,3 @@ def create_movie(title, genre, rating):
 # -----------------------------------------
 # ------------- WAVE 5 --------------------
 # -----------------------------------------
-
