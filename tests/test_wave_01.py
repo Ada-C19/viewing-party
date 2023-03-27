c1e06933d453646b3f471bb820021c17512e684e
Winslow <owedwards@gmail.com>
2023-03-27 12:02:08 -0400

    Finished add_to_watched + updated add_to_watchlist

--- tests/test_wave_01.py
+++ tests/test_wave_01.py
@@ -54,7 +54,6 @@ def test_create_no_rating_movie():
     # Assert
     assert new_movie is None
 
-@pytest.mark.skip()
 def test_adds_movie_to_user_watched():
     # Arrange
     movie = {
@@ -75,7 +74,6 @@ def test_adds_movie_to_user_watched():
     assert updated_data["watched"][0]["genre"] == GENRE_1
     assert updated_data["watched"][0]["rating"] == RATING_1
 
-@pytest.mark.skip()
 def test_adds_movie_to_non_empty_user_watched():
     # Arrange
     movie = {
@@ -95,7 +93,6 @@ def test_adds_movie_to_non_empty_user_watched():
     assert movie in updated_data["watched"]
     assert FANTASY_2 in updated_data["watched"]
 
-@pytest.mark.skip()
 def test_adds_movie_to_user_watchlist():
     # Arrange
     movie = {
@@ -116,7 +113,6 @@ def test_adds_movie_to_user_watchlist():
     assert updated_data["watchlist"][0]["genre"] == GENRE_1
     assert updated_data["watchlist"][0]["rating"] == RATING_1
 
-@pytest.mark.skip()
 def test_adds_movie_to_non_empty_user_watchlist():
     # Arrange
     movie = {
--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,14 +1,6 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-#     1. Create a function named  `create_movie`. This function and all subsequent functions should be in `party.py`. `create_movie` should...
-
-# - take three parameters: `title`, `genre`, `rating`
-# - If those three attributes are truthy, then return a dictionary. This dictionary should...
-#   - Have three key-value pairs, with specific keys
-#   - The three keys should be `"title"`, `"genre"`, and `"rating"`
-#   - The values of these key-value pairs should be appropriate values
-# - If `title` is falsy, `genre` is falsy, or `rating` is falsy, this function should return `None`
     movies_dict = {}
     if not title or not genre or not rating:
         return None
@@ -18,6 +10,54 @@ def create_movie(title, genre, rating):
         movies_dict["rating"] = rating 
     return movies_dict
 
+def add_to_watched(user_data, movie):
+    watched_list = user_data["watched"]
+    is_valid = True
+
+    ### checking that watched_list is not empty
+    if watched_list:
+    ### checking that title is not already in watched_list keys
+        for movie_dict in watched_list:
+            if movie_dict["title"] == movie["title"]:
+                is_valid = False
+    
+    ### if movie is not duplicate, append to watched_list
+    if is_valid:
+        watched_list.append(movie)
+
+    return user_data
+
+def add_to_watchlist(user_data, movie):  
+# 3. Create a function named `add_to_watchlist`. This function should...
+
+# - take two parameters: `user_data`, `movie`
+#   - the value of `user_data` will be a dictionary with a key `"watchlist"`, and a value which is a list of dictionaries representing the movies the user wants to watch
+#     - An empty list represents that the user has no movies in their watchlist
+#   - the value of `movie` will be a dictionary in this format:
+#     - ```python
+#       {
+#         "title": "Title A",
+#         "genre": "Horror",
+#         "rating": 3.5
+#       }
+#       ```
+# - add the `movie` to the `"watchlist"` list inside of `user_data`
+# - return the `user_data`
+    future_watch_list = user_data["watchlist"]
+    is_valid = True
+
+    ### checking that future_watch_list is not empty
+    if future_watch_list:
+    ### checking that title is not already in future_watch_list keys
+        for movie_dict in future_watch_list:
+            if movie_dict["title"] == movie["title"]:
+                is_valid = False
+    
+    ### if movie is not duplicate, append to watched_list
+    if is_valid:
+        future_watch_list.append(movie)
+
+    return user_data
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
