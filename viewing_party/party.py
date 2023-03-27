959567a9d7d870afa4b3be81b8f5ae0b1a75ace2
Winslow <owedwards@gmail.com>
2023-03-27 11:21:55 -0400

    Completed 'create_movie' function

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,7 +1,22 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    pass
+#     1. Create a function named  `create_movie`. This function and all subsequent functions should be in `party.py`. `create_movie` should...
+
+# - take three parameters: `title`, `genre`, `rating`
+# - If those three attributes are truthy, then return a dictionary. This dictionary should...
+#   - Have three key-value pairs, with specific keys
+#   - The three keys should be `"title"`, `"genre"`, and `"rating"`
+#   - The values of these key-value pairs should be appropriate values
+# - If `title` is falsy, `genre` is falsy, or `rating` is falsy, this function should return `None`
+    movies_dict = {}
+    if not title or not genre or not rating:
+        return None
+    else:
+        movies_dict["title"] = title
+        movies_dict["genre"] = genre
+        movies_dict["rating"] = rating 
+    return movies_dict
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
