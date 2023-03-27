48617c8e68e508246e91a6dd44b95295e598c85a
An Ba <ngwin.ab@gmail.com>
2023-03-27 11:47:05 -0400

    finished add_to_watched function

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -7,6 +7,11 @@ def create_movie(title, genre, rating):
         return {"title": title, 
                 "genre": genre,
                 "rating": rating}
+    
+def add_to_watched(user_data, movie):
+    user_data["watched"].append(movie)
+    print(user_data)
+    return user_data
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
