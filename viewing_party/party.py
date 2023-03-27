52b46d6f6bc3c14f19e64f802e1785b7e712438a
nadia <reach.nadia.saria@gmail.com>
2023-03-27 16:35:21 -0700

    Finishes wave 01 incl updates to test file

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -11,7 +11,29 @@ def create_movie(title, genre, rating):
         movie_dict["rating"] = rating
         return movie_dict
 
+def add_to_watched(user_data, movie):
+    user_data["watched"].append(movie)
+    return user_data
 
+def add_to_watchlist(user_data, movie):
+    user_data["watchlist"].append(movie)
+    return user_data
+
+def watch_movie(user_data, title):
+    """
+    watch_movie(user_data, title)
+		if title is not in user’s watchlist
+			return user_data
+		else
+			remove that movie from watchlist
+			add it to watched
+			return user_data
+    """
+    for i in range(len(user_data["watchlist"])):
+        if user_data["watchlist"][i]["title"] == title:
+            user_data["watched"].append(user_data["watchlist"][i])
+            user_data["watchlist"].remove(user_data["watchlist"][i])
+    return user_data
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
