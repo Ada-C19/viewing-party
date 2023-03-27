c982e51942683cf4d75d370a279ec5208d611c42
Dee Williams <deejmwilliams@gmail.com>
2023-03-27 14:29:09 -0400

    Add functionality for Wave 2; all tests pass

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -7,6 +7,20 @@ def create_movie(title, genre, rating):
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
 
+def get_watched_avg_rating(user_data):
+    watched = user_data["watched"]
+
+    if not watched: return 0
+
+    return sum([watched[i]["rating"] for i in range(len(watched))]) / len(watched)
+
+def get_most_watched_genre(user_data):
+    watched = user_data["watched"]
+
+    if not watched: return None
+    
+    genre_list = [watched[i]["genre"] for i in range(len(watched))]
+    return max(genre_list, key=genre_list.count)
 
 # -----------------------------------------
 # ------------- WAVE 3 --------------------
