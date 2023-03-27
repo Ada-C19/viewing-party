584d88b72786cddfc64e32d556bad630abaafa42
Limary Gonzalez <limaryg11@gmail.com>
2023-03-27 11:48:36 -0700

    first part of wave01

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,7 +1,22 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    pass
+    # if the parameters are truthy; return dictionary
+    # 3 keys: title, genre, rating
+
+    if title == False or genre == False or rating == False:
+        return None
+    
+    movie_ratings = {}
+
+    movie_ratings["title"] = title
+    movie_ratings["genre"] = genre
+    movie_ratings["rating"] = rating
+
+    return movie_ratings
+
+
+
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
