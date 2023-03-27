6640d3a20608c866c2a0e691eaf38899c4f60e36
Angie <caritocon@gmail.com>
2023-03-27 13:29:14 -0700

    function 3 and tests

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -13,8 +13,6 @@ def create_movie(title, genre, rating):
     # Return the dictionary
     return movie
 
-# print(create_movie("Seahawks", "sportsball", 1))
-
 def add_to_watched(user_data, movie):
     movie_data = {
         "watched": []
@@ -22,10 +20,21 @@ def add_to_watched(user_data, movie):
 
     movie_data["watched"].append(movie)
     return movie_data
+
+def add_to_watchlist(user_data, movie):
+
+    if not movie:
+        return user_data
+    
+    user_data["watchlist"].append(movie)
+    
+    return user_data
+print(add_to_watchlist({"watchlist": ["FANTASY_2"]}, {"title": "MOVIE_TITLE_1", "genre": "GENRE_1",
+        "rating": "RATING_1"}))
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
-    
+
 
 # -----------------------------------------
 # ------------- WAVE 3 --------------------
