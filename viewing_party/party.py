83c3dfe65d8226c301bb343d32174c43f013c6cf
Elizabeth Zachary <elizabethzachary96@gmail.com>
2023-03-27 15:22:26 -0400

    implemented functionality for get_watched_avg_rating()

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -7,6 +7,19 @@ def create_movie(title, genre, rating):
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
 
+def get_watched_avg_rating(user_data):
+    if user_data["watched"]:
+        watched_list = user_data["watched"]
+        ratings_total = 0
+
+        for movie in watched_list:
+            ratings_total += movie["rating"]
+        
+        avg = ratings_total / len(watched_list)
+
+        return avg
+    return 0.0
+
 
 # -----------------------------------------
 # ------------- WAVE 3 --------------------
