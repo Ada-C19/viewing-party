b55d8345d7930f47a763359d39d69477137231a0
Katherine Guarnizo <guarnizo.katherine.5@gmail.com>
2023-03-28 17:58:04 -0400

    Update assert statement on test_moves_movie_from_watchlist_to_empty_watched

--- tests/test_wave_01.py
+++ tests/test_wave_01.py
@@ -158,7 +158,7 @@ def test_moves_movie_from_watchlist_to_empty_watched():
     # Assert
     assert len(updated_data["watchlist"]) == 0
     assert len(updated_data["watched"]) == 1
-    # assert updated_data["watched"]["title"]== MOVIE_TITLE_1
+    assert updated_data["watched"][0]["title"]== MOVIE_TITLE_1
     
 
 # @pytest.mark.skip()
@@ -179,7 +179,7 @@ def test_moves_movie_from_watchlist_to_watched():
     # Assert
     assert len(updated_data["watchlist"]) == 1
     assert len(updated_data["watched"]) == 2
-    # assert updated_data["watched"][-1] == HORROR_1
+    assert updated_data["watched"][-1] == HORROR_1
     
 
 # @pytest.mark.skip()
