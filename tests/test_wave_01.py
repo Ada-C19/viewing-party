63b0b6a6d3702b34eebdd195442e6a3f1aeb6f66
Paige <ph.paigee@gmail.com>
2023-03-28 13:37:36 -0700

    wrote get_watched_avg_rating function for wave 2

--- tests/test_wave_01.py
+++ tests/test_wave_01.py
@@ -159,10 +159,6 @@ def test_moves_movie_from_watchlist_to_empty_watched():
     assert len(updated_data["watchlist"]) == 0
     assert len(updated_data["watched"]) == 1
     
-    raise Exception("Test needs to be completed.")
-    # *******************************************************************************************
-    # ****** Add assertions here to test that the correct movie was added to "watched" **********
-    # *******************************************************************************************
 
 @pytest.mark.skip()
 def test_moves_movie_from_watchlist_to_watched():
