72d3b0bd2b5443e7bbc0782a7b9cbd6e06e6650b
Tigist Woldearegai <tigistlina@gmail.com>
2023-03-27 20:39:06 -0500

    defined add_t0_watched function.

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -2,14 +2,14 @@
 
 def create_movie(title, genre, rating):
     if title and genre and rating:
-
         return {"title": title, "genre": genre, "rating": rating}
     else: 
         return None
 
-# -----------------------------------------
-# ------------- WAVE 2 --------------------
-# -----------------------------------------
+def add_to_watched(user_data, movie):
+    user_data["watched"].append(movie)
+    return user_data
+
 
 
 # -----------------------------------------
@@ -20,6 +20,7 @@ def create_movie(title, genre, rating):
 # -----------------------------------------
 # ------------- WAVE 4 --------------------
 # -----------------------------------------
+
 
 # -----------------------------------------
 # ------------- WAVE 5 --------------------
