f8d5bbe9591127d96f393ad8902c93a3f8b509fa
Angela <echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"')  /Users/an5073/.zprofile>
2023-03-28 14:11:12 -0400

    Wave 3 get_unique_watched incomplete

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -103,8 +103,35 @@ def get_most_watched_genre(user_data):
 # -----------------------------------------
 # ------------- WAVE 3 --------------------
 # -----------------------------------------
+# 1. Create a function named `get_unique_watched`. This function should...
 
-        
+# - take one parameter: `user_data`
+#   - the value of `user_data` will be a dictionary with a `"watched"` list of movie dictionaries, and a `"friends"`
+#     - This represents that the user has a list of watched movies and a list of friends
+#     - The value of `"friends"` is a list
+#     - Each item in `"friends"` is a dictionary. This dictionary has a key `"watched"`, which has a list of movie dictionaries.
+#     - Each movie dictionary has a `"title"`.
+# - Consider the movies that the user has watched, and consider the movies that their friends have watched. Determine which movies the user has watched, but none of their friends have watched.
+# - Return a list of dictionaries, that represents a list of movies
+
+def get_unique_watched(user_data):
+    watched_list = user_data["watched"]
+    friends_watched_list = user_data["friends"]
+    watched_list_set = set()
+    friends_watched_list_set = set()
+
+    for movie_dict in watched_list:
+        watched_list_set.add(movie_dict["title"])
+        print(f"watched_list_set = {watched_list_set}")
+    
+    for friend in friends_watched_list:
+        for movie_dict in friend:
+            print(f"movie_dict in friend = {movie_dict}")
+            friends_watched_list_set.add(movie_dict["title"])
+            print(f"friends_watched_list_set = {friends_watched_list_set}")
+    
+    return watched_list_set - friends_watched_list_set
+
 # -----------------------------------------
 # ------------- WAVE 4 --------------------
 # -----------------------------------------
