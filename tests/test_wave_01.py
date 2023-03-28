0f3e06bc291c4b02b759340b3cdae0234d330af2
jasmine <jtmsoto@gmail.com>
2023-03-27 17:49:15 -0700

    Add watch_movie function and passed 9 out of 11 tests

--- tests/test_wave_01.py
+++ tests/test_wave_01.py
@@ -158,8 +158,12 @@ def test_moves_movie_from_watchlist_to_empty_watched():
     # Assert
     assert len(updated_data["watchlist"]) == 0
     assert len(updated_data["watched"]) == 1
-    
-    raise Exception("Test needs to be completed.")
+    assert updated_data["watched"][0] == {
+            "title": MOVIE_TITLE_1,
+            "genre": GENRE_1,
+            "rating": RATING_1
+        }
+    # raise Exception("Test needs to be completed.")
     # *******************************************************************************************
     # ****** Add assertions here to test that the correct movie was added to "watched" **********
     # *******************************************************************************************
--- viewing_party/party.py
+++ viewing_party/party.py
@@ -25,6 +25,16 @@ def add_to_watchlist(user_data, movie):
 
     user_data["watchlist"].append(movie)
     return user_data
+
+def watch_movie(user_data, title):
+    # Iterates through every movie dict stored in watchlist
+    for i in range(len(user_data["watchlist"])):
+        # Checks to see if the title is in the user's watchlist. If it is,
+        # it adds movie dict to watched, and removes it from watchlist.
+        if title == user_data["watchlist"][i]["title"]:
+            user_data["watched"].append(user_data["watchlist"][i])
+            del user_data["watchlist"][i]
+    return user_data
  
 
 # -----------------------------------------
