638d1987ad6120ba000944977ab69ea8416a3311
Elizabeth Zachary <elizabethzachary96@gmail.com>
2023-03-28 17:22:04 -0400

    fixed functionality for get_most_watched_genre

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -44,49 +44,35 @@ def get_watched_avg_rating(user_data):
 
 def get_most_watched_genre(user_data):
     # first validate that user_data["watched"] is truthy, else return None
-    if user_data["watched"]:
+    if not user_data["watched"]:
+        return None
         
-        # store the nested list in its own variable for ease of access
-        watched_list = user_data["watched"]
+    # store the nested list in its own variable for ease of access
+    watched_list = user_data["watched"]
 
-        # declare total rating variables for each genre
-        fantasy_ratings_total = 0
-        action_ratings_total = 0
-        intrigue_ratings_total = 0
+    # declare counter variables for the amount of movies in each genre
+    num_fantasy_movies = 0
+    num_action_movies = 0
+    num_intrigue_movies = 0
 
-        # declare counter variables for the amount of movies in each genre
-        num_fantasy_movies = 0
-        num_action_movies = 0
-        num_intrigue_movies = 0
+    # iterate through the watched list to add ratings and genres to their respective variables
+    for movie in watched_list:
+        if movie["genre"] == "Fantasy":
+            num_fantasy_movies += 1
+        elif movie["genre"] == "Action":
+            num_action_movies += 1
+        elif movie["genre"] == "Intrigue":
+            num_intrigue_movies += 1
 
-        # iterate through the watched list to add ratings and genres to their respective variables
-        for movie in watched_list:
-            if movie["genre"] == "Fantasy":
-                fantasy_ratings_total += movie["rating"]
-                num_fantasy_movies += 1
-            elif movie["genre"] == "Action":
-                action_ratings_total += movie["rating"]
-                num_action_movies += 1
-            elif movie["genre"] == "Intrigue":
-                intrigue_ratings_total += movie["rating"]
-                num_intrigue_movies += 1
-            
-        # calculate the averages for each genre type
-        fantasy_avg = fantasy_ratings_total / num_fantasy_movies
-        action_avg = action_ratings_total / num_action_movies
-        intrigue_avg = intrigue_ratings_total / num_intrigue_movies
+    most_watched = max(num_action_movies, num_fantasy_movies, num_intrigue_movies)
 
-        # retrieve the max rating across all genre averages
-        highest_avg_rating = max(fantasy_avg, action_avg, intrigue_avg)
-
-        # find the corresponding genre to the max rating
-        if fantasy_avg == highest_avg_rating:
-            return "Fantasy"
-        elif action_avg == highest_avg_rating:
-            return "Action"
-        elif intrigue_avg == highest_avg_rating:
-            return "Intrigue"
-    return None
+    # find the corresponding genre to the max rating
+    if num_action_movies == most_watched:
+        return "Action"
+    elif num_fantasy_movies == most_watched:
+        return "Fantasy"
+    elif num_intrigue_movies == most_watched:
+        return "Intrigue"
 
 # -----------------------------------------
 # ------------- WAVE 3 --------------------
