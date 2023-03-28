7c4f20637f74b5a1c411032560b31898ec57e17f
Anh Huynh <avh2@uw.edu>
2023-03-28 11:37:35 -0700

    made minor change

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,22 +1,16 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    # if the parameters are truthy; return dictionary
-    # 3 keys: title, genre, rating
 
-    #initialize empty dictionary 
     movie_ratings = {}
 
-    #if any of parameters is Falsy, return None
     if not title or not genre or not rating:
         return None
     
-    #otherwise, populate dictionary
     movie_ratings["title"] = title
     movie_ratings["genre"] = genre
     movie_ratings["rating"] = rating
     
-    #return dictionary
     return movie_ratings
 
 
