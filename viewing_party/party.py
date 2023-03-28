18c6449239633f87bdd73daef8c0222f1d5e33b4
Megan Gonzalez <meganmgonzalez18@icloud.com>
2023-03-28 11:51:22 -0400

    Test Wave 1 Complete

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,7 +1,39 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    pass
+
+    if bool(title) and bool(genre) and bool(rating) == True:
+        user_data = {}
+        user_data['title'] = title
+        user_data['genre'] = genre
+        user_data['rating'] = rating
+        return user_data
+    
+    return None
+
+def add_to_watched(user_data, movie):
+
+    user_data['watched'].append(movie)
+
+    return user_data
+
+def add_to_watchlist(user_data, movie):
+    
+    user_data['watchlist'].append(movie)
+    return user_data
+
+
+def watch_movie(user_data, title):
+
+    for movie_data in user_data['watchlist']:
+        if title == movie_data['title']:
+            user_data['watchlist'].remove(movie_data)
+            user_data['watched'].append(movie_data)
+            return user_data
+    
+    if title not in user_data['watchlist']:
+        return user_data
+
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
