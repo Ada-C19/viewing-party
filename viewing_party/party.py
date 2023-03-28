1be337c9b23a7bd169a0b44a41072c763580fde0
Ann <anntuminello@gmail.com>
2023-03-27 18:34:21 -0700

    Wave one complete minus two tests to be written.

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -14,11 +14,27 @@ def create_movie(title, genre, rating):
 def add_to_watched(user_data, movie):
     watched_list = user_data["watched"]
     watched_list.append(movie)
+    user_data["watched"] = watched_list
     return user_data
+
 
 def add_to_watchlist(user_data, movie):
     watchlist = user_data["watchlist"]
     watchlist.append(movie)
+    user_data["watchlist"] = watchlist
+    return user_data
+
+
+def watch_movie(user_data, title):
+    watchlist = user_data["watchlist"]
+    watched_list = user_data["watched"]
+    for movie in watchlist:
+        if movie["title"] == title:
+            watchlist.remove(movie)
+            watched_list.append(movie)
+            user_data["watchlist"] = watchlist
+            user_data["watched"] = watched_list
+            break
     return user_data
     
 
