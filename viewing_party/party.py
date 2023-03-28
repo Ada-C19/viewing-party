fd2d539fad41d6df6cc1ea811999b6ddad7c0091
Moyo Moz <moyotornai@gmail.com>
2023-03-28 00:46:29 -0700

    wave_3 get_unique_watched function finished and test passed

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,7 +1,7 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    pass
+    pass # can you
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
@@ -11,6 +11,17 @@ def create_movie(title, genre, rating):
 # -----------------------------------------
 # ------------- WAVE 3 --------------------
 # -----------------------------------------
+def get_unique_watched(user_data):
+    user_watched_movies = {movie['title']: movie for movie in user_data['watched']}
+    friends_watched_movies = set()
+
+    for friend in user_data['friends']:
+        for movie in friend['watched']:
+            friends_watched_movies.add(movie['title'])
+
+    unique_watched_movies = set(user_watched_movies.keys()).difference(friends_watched_movies)
+    return [user_watched_movies[movie] for movie in unique_watched_movies]
+
 
         
 # -----------------------------------------
