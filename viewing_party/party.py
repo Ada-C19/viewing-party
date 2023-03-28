921c87dd7d9d4710d92034e4661fb1fecf38ab4c
Moyo Moz <moyotornai@gmail.com>
2023-03-28 01:26:00 -0700

    wave_3 get_friends_unique_watched function  created and test passed

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,8 +1,7 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    pass # can you
-=======
+    pass 
 def create_movie(movie_title, genre, rating):
     if movie_title and genre and rating:
         return {
@@ -44,6 +43,17 @@ def get_unique_watched(user_data):
     unique_watched_movies = set(user_watched_movies.keys()).difference(friends_watched_movies)
     return [user_watched_movies[movie] for movie in unique_watched_movies]
 
+
+def get_friends_unique_watched(user_data):
+    user_watched_movies = {movie['title']: movie for movie in user_data['watched']}
+    friends_watched_movies = {}
+
+    for friend in user_data['friends']:
+        for movie in friend['watched']:
+            friends_watched_movies[movie['title']] = movie
+
+    unique_friends_watched_movies = set(friends_watched_movies.keys()).difference(user_watched_movies.keys())
+    return [friends_watched_movies[movie] for movie in unique_friends_watched_movies]
 
         
 # -----------------------------------------
