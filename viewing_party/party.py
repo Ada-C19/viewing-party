13a0bc20289281ae2d9c3b47c332686280d993bb
Kat <kmunson13@gmail.com>
2023-03-28 13:48:32 -0400

    Add friends_uniquie_movies

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -63,7 +63,6 @@ def get_most_watched_genre(user_data):
 # -----------------------------------------
 # ------------- WAVE 3 --------------------
 # -----------------------------------------
-<<<<<<< HEAD
 def get_friends_unique_watched(user_data):
 
     # user_titles = []
@@ -79,38 +78,6 @@ def get_friends_unique_watched(user_data):
             
     
     
-=======
-def get_unique_watched(user_data):
-    result = []
-    # for user_movie in user_data['watched']:
-    #     for item in user_data['friends']:
-    #         if user_movie in item['watched']:
-    #             uniqe_movie.append(user_movie)
-    #     # for friend in user_data['friends']:
-    #     #     for friend_movie in friend['watched']:
-    #     #         if friend_movie != user_movie:
-    #                 # uniqe_movie.append(friend_movie)
-
-    # for friends in user_data['friends']:
-    #     for friends_watched_movies in friends['watched']:
-    #         for item in user_data['watched']:
-    #             if item not in friends_watched_movies:
-    #                 result.append(item)
-
-    # return result
-    friends_movies_watch = []
-    if len(user_data['watched']) == 0:
-        return []
-    for friend in user_data['friends']:
-        for friends_watched_movie in friend['watched']:
-            friends_movies_watch.append(friends_watched_movie)
-
-    for movie in user_data['watched']:
-        if movie not in friends_movies_watch:
-            result.append(movie)
-
-    return result
->>>>>>> 4c0609c277578d9718c7bbbe573ff332ed47d0eb
 
 # -----------------------------------------
 # ------------- WAVE 4 --------------------
