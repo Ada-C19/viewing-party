5d30c3632412f634107a7bc8beeb1c01898964bc
Trang Tran <trantrang181197@gmail.com>
2023-03-28 13:14:05 -0700

    add wave_1, pass tests

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,8 +1,36 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    pass
+    if title and genre and rating:
+        return {
+            "title": title,
+            "genre": genre,
+            "rating": rating
+            }
+    
+    return None
 
+def add_to_watched(user_data, movie):
+    if movie not in user_data.get("watched"):
+        user_data.get("watched").append(movie)
+
+    return user_data
+
+def add_to_watchlist(user_data, movie):
+    if movie not in user_data.get("watchlist"):
+        user_data.get("watchlist").append(movie)
+
+    return user_data
+
+def watch_movie(user_data, title):
+    movies = user_data.get("watchlist")
+
+    for movie in movies:
+        if title == movie["title"]:
+            movies.remove(movie)
+            user_data.get("watched").append(movie)
+    
+    return user_data
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
