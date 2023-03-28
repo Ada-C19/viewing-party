a9ab03bdd73f9a712eb9c37ab51e1943783aae29
Ana Silvera <anasilverx@gmail.com>
2023-03-27 18:25:22 -0700

    Fixed create_movie function and created add_to_watched funtion

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -3,12 +3,21 @@
 def create_movie(title, genre, rating):
     
     new_movie = {}
-    if title or genre or rating:
-        new_movie["title"] = title
-        new_movie["genre"] = genre
-        new_movie["rating"] = rating 
-        return new_movie
-    return None
+    if not (title and genre and rating):
+        return None 
+    new_movie["title"] = title
+    new_movie["genre"] = genre
+    new_movie["rating"] = rating 
+    return new_movie
+    
+
+def add_to_watched(user_data, movie):
+
+    user_data = {'watched': []}
+
+    user_data['watched'].append(movie)
+
+    return user_data 
 
 
 
