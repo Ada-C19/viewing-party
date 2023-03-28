6d24d47c74f66cc732485488968f9692900e6bc9
April Zhang <aprilzh23@gmail.com>
2023-03-27 18:41:07 -0700

    add to watchlist

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -13,13 +13,18 @@ def create_movie(title, genre, rating):
 
 def add_to_watched(user_data, movie):
 
-    user_data = {'watched': []}
+    # user_data = {'watched': []}
 
     user_data['watched'].append(movie)
 
     return user_data 
 
+def add_to_watchlist(user_data, movie):
+    user_data['watchlist'].append(movie)
+    return user_data
 
+def watch_movie(user_data, title):
+    
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
