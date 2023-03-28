81f8c663087075516be64b50fa534d6782fde2e4
Areeg Jibrin <areegjibrin@gmail.com>
2023-03-28 17:02:56 -0500

    defined get_available_recs and passed all tests.

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -92,9 +92,16 @@ def get_friends_unique_watched(user_data):
     return user_not_watched
 
 # ------------- WAVE 4 --------------------
+def get_available_recs(user_data):
+    unique_movies = get_friends_unique_watched(user_data)
+    recommended_movies = []
+
+    for movie in unique_movies:
+        if movie["host"] in user_data["subscriptions"]:
+            recommended_movies.append(movie)
+    return recommended_movies
 
 
 # -----------------------------------------
 # ------------- WAVE 5 --------------------
 # -----------------------------------------
-
