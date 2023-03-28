356f7d99dfa672e51e72804ece97e6026bf9c6f6
Ana Villalobos <ana.villalobos309@gmail.com>
2023-03-28 11:18:19 -0700

    Defined function 4 in wave 1 and passed all associated tests in test_wave_01

--- tests/test_wave_01.py
+++ tests/test_wave_01.py
@@ -140,7 +140,7 @@ def test_adds_movie_to_non_empty_user_watchlist():
     assert movie in updated_data["watchlist"]
     assert FANTASY_2 in updated_data["watchlist"]
 
-@pytest.mark.skip()
+# @pytest.mark.skip()
 def test_moves_movie_from_watchlist_to_empty_watched():
     # Arrange
     janes_data = {
@@ -158,7 +158,7 @@ def test_moves_movie_from_watchlist_to_empty_watched():
     # Assert
     assert len(updated_data["watchlist"]) == 0
     assert len(updated_data["watched"]) == 1
-    assert updated_data["watched"][0] == [MOVIE_TITLE_1]
+    assert updated_data["watched"][0]["title"] == MOVIE_TITLE_1
     assert MOVIE_TITLE_1 not in updated_data["watchlist"]
 
 
@@ -167,7 +167,7 @@ def test_moves_movie_from_watchlist_to_empty_watched():
     # ****** Add assertions here to test that the correct movie was added to "watched" **********
     # *******************************************************************************************
 
-@pytest.mark.skip()
+# @pytest.mark.skip()
 def test_moves_movie_from_watchlist_to_watched():
     # Arrange
     movie_to_watch = HORROR_1
@@ -193,7 +193,7 @@ def test_moves_movie_from_watchlist_to_watched():
     # ****** Add assertions here to test that the correct movie was added to "watched" **********
     # *******************************************************************************************
 
-@pytest.mark.skip()
+# @pytest.mark.skip()
 def test_does_nothing_if_movie_not_in_watchlist():
     # Arrange
     movie_to_watch = HORROR_1
--- viewing_party/party.py
+++ viewing_party/party.py
@@ -22,13 +22,19 @@ def add_to_watchlist(user_data, movie):
     user_data["watchlist"].append(movie)
     return user_data
 
-
-#function 2
-def add_to_watched(user_data, movie):
-
-    user_data["watched"].append(movie)
-
+#4th funcyion in wave_01
+def watch_movie(user_data, title):
+    index = 0
+    for index in range(len(user_data["watchlist"])):
+        if title in (user_data["watchlist"][index]["title"]):
+            move_movie = user_data["watchlist"].pop(index)
+            #to move to end of list
+            user_data["watched"].append(move_movie)
+            #to move to begiiniing of list
+            # user_data["watched"].insert(0, move_movie)
+            return user_data
     return user_data
+
 
 
 # -----------------------------------------
