18f36e599f45f449724008d08fc407e0860470bc
Sarah Watkins <sarahwwatkins@gmail.com>
2023-03-28 11:46:56 -0400

    Wrote add to watched

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -7,6 +7,9 @@ def create_movie(title, genre, rating):
 
     return {"title" : title, "genre" : genre, "rating" : rating}
 
+def add_to_watched(user_data, movie):
+    user_data["watched"].append(movie)
+    return user_data
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
