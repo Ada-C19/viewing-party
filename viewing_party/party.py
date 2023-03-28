6f81123db06d544b5bd54b8a09dbd0cfa129ca5d
Trang Tran <trantrang181197@gmail.com>
2023-03-28 14:11:10 -0700

    test wave_02 and functions wave_02 added

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -34,13 +34,54 @@ def watch_movie(user_data, title):
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
+def get_watched_avg_rating(user_data):
+    rating = []
+    try:
+        for movie in user_data["watched"]:
+            rating.append(movie["rating"])
+        return sum(rating) / len(rating)
+    except ZeroDivisionError:
+        return 0.0
+    except KeyError: # Incase the key "watched" is missing
+        return None
 
+def get_most_watched_genre(user_data):
+    genre = {}
 
+    try:
+        for movie in user_data.get("watched"):
+            if movie["genre"] not in genre:
+                genre[movie["genre"]] = 1
+
+            genre[movie["genre"]] += 1
+            print(genre)
+    except TypeError:
+        return None
+    
+    if genre:
+        return max(genre, key=genre.get)
+    
+    return None
 # -----------------------------------------
 # ------------- WAVE 3 --------------------
-# -----------------------------------------
+# ------------------------------------------
+def get_unique_watched(user_data):
+    user_data = {
+        "watched": [{movie}, {movie}, ..],
+        "friends": [{watched: [{movie}]}, {watched}, {watched}]
+    }
+    
+    watched = set()
+    friends_watched = set()
 
-        
+    for movie in user_data.get("watched"):
+        watched.add(movie)
+    
+    for friend in user_data.get("friends"):
+        friends_watched.extend(friend.get("watched"))
+    
+    return list(watched - friends_watched)
+
 # -----------------------------------------
 # ------------- WAVE 4 --------------------
 # -----------------------------------------
