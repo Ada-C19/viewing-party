d9a5a953092eafbe46385187c0d6a35c947fc2f1
chickenandegg <tei.villanueva@gmail.com>
2023-03-28 15:20:31 -0700

    pass 2 tests in wave_03

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -61,6 +61,7 @@ def get_watched_avg_rating(user_data):
 
     # calculate average
 
+
 def get_most_watched_genre(user_data):
     user_genres = {}
     most_watched = None
@@ -69,28 +70,60 @@ def get_most_watched_genre(user_data):
     # Returns None if watched list is empty
     if not user_data["watched"]:
         return None
-    
-    # Iterates through each movie in watched list and adds 
-    # each genre to a user_genres dictionary with an initial value of 1. 
+
+    # Iterates through each movie in watched list and adds
+    # each genre to a user_genres dictionary with an initial value of 1.
     # If the genre already exists in user_genres, it +1 to the value.
     for movie in user_data["watched"]:
         if movie["genre"] not in user_genres:
             user_genres[movie["genre"]] = 1
         else:
             user_genres[movie["genre"]] += 1
-    
+
     # Iterates through the genres in user_genres to find most watched
     for genre in user_genres:
         if user_genres[genre] > times_watched:
             most_watched = genre
             times_watched = user_genres[genre]
-    
+
     return most_watched
 
 
-# -----------------------------------------
 # ------------- WAVE 3 --------------------
-# -----------------------------------------
+def get_unique_watched(user_data):
+
+    # initialize emoty list
+    # nested dictionary - friends - watched
+    # append list
+
+    friends_list = []
+
+    for each_friend_dict in user_data.get("friends", []):
+        for each_movie_dict in each_friend_dict.get("watched", []):
+            friends_list.append(each_movie_dict)
+
+# initialize a set of my friend's list title
+# loop through the friends_list:
+# get value of title
+# if there's title existent, add the title to the set -
+
+    friends_titles = set()
+    for each_movie_dict in friends_list:
+        title = each_movie_dict.get("title")
+        if title:
+            friends_titles.add(title)
+
+# loop through the user_data["watched"]
+# empty list - store the list -- unique dict
+
+    unique_movies = []
+    for each_movie_dict in user_data["watched"]:
+        title = each_movie_dict.get("title", [])
+        # if my title is in their list then add the dictionary to my list
+        if title and title not in friends_titles:
+            unique_movies.append(each_movie_dict)
+
+    return unique_movies
 
 
 # -----------------------------------------
