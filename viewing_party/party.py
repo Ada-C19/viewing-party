5f11cc5a04063c8c8a1535182292fd7e7702b8d0
Airis <airis.wang2014@fgvmail.br>
2023-03-28 16:10:11 -0400

    defined get_most_watched_genre() and passed all tests

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -38,6 +38,23 @@ def get_watched_avg_rating(user_data):
     average_rating = average_rating / len(user_data["watched"])
     return average_rating
 
+def get_most_watched_genre(user_data):
+    genres = {}
+    max_count = 0
+
+    for movie in user_data["watched"]:
+        if movie["genre"] not in genres:
+            genres[movie["genre"]] = 1
+        else:
+            genres[movie["genre"]] += 1
+
+    for count in genres.values():
+        if count > max_count:
+            max_count = count
+
+    for key, value in genres.items():
+        if value == max_count:
+            return key
 
 # -----------------------------------------
 # ------------- WAVE 3 --------------------
