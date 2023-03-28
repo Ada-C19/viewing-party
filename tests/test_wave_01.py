7daad9982edce61d49d6522ecfcca4ad7cf287a6
Monica <monicalagdaan@hotmail.com>
2023-03-28 14:06:31 -0700

    added assert statements to test 1

--- tests/test_wave_01.py
+++ tests/test_wave_01.py
@@ -158,8 +158,11 @@ def test_moves_movie_from_watchlist_to_empty_watched():
     # Assert
     assert len(updated_data["watchlist"]) == 0
     assert len(updated_data["watched"]) == 1
+    assert MOVIE_TITLE_1 in updated_data["watchlist"]
     
-    raise Exception("Test needs to be completed.")
+    # Assert added above
+    # raise Exception("Test needs to be completed.")
+
     # *******************************************************************************************
     # ****** Add assertions here to test that the correct movie was added to "watched" **********
     # *******************************************************************************************
@@ -182,8 +185,11 @@ def test_moves_movie_from_watchlist_to_watched():
     # Assert
     assert len(updated_data["watchlist"]) == 1
     assert len(updated_data["watched"]) == 2
-    
-    raise Exception("Test needs to be completed.")
+    assert movie_to_watch in updated_data["watched"]
+
+    # Added assert above
+    #raise Exception("Test needs to be completed.")
+
     # *******************************************************************************************
     # ****** Add assertions here to test that the correct movie was added to "watched" **********
     # *******************************************************************************************
