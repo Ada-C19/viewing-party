54284c90fbdd44d5168e2c3af02dc1e1807988a0
celina-barron <celinahas@gmail.com>
2023-03-27 19:35:31 -0700

    passing first 8 tests in wave 1

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,7 +1,29 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    pass
+    movie = {}
+    if not title or not genre or not rating:
+        return None
+    else:
+        movie.update({"title" : title, "genre" : genre, "rating" : rating})
+        return movie
+
+def add_to_watched(user_data, movie):
+    # user_data = {
+    #     "watched" : [movie dictionaries]
+    # }
+    
+    # movie = {
+    #     "title": "Title A",
+    #     "genre": "Horror",
+    #     "rating": 3.5
+    #     }
+    user_data["watched"].append(movie)
+    return user_data
+
+def add_to_watchlist(user_data, movie):
+    user_data["watchlist"].append(movie)
+    return user_data
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
