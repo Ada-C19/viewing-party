b274b2ea824483f26b4c59ed1764e42ea8b6b675
Samantha Hurtado <samanthahurtado@Samanthas-MacBook-Pro.local>
2023-03-28 09:26:12 -0700

    wave 1 done and 2 started

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -19,13 +19,34 @@ def add_to_watched(user_data, movie):
     return user_data
 
 
+def add_to_watchlist(user_data, movie):
+    user_data["watchlist"].append(movie)
+    return user_data
 
+def watch_movie(user_data, title):
+    for movie in user_data["watchlist"]:
+        if movie["title"] == title:
+            user_data["watchlist"].remove(movie)
+            user_data["watched"].append(movie)
+            return user_data
+        
+    return user_data
     
+
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
 
 
+def get_watched_avg_rating(user_data):
+    if not user_data["watched"]:
+        return 0
+    
+    running_total = 0
+    for movie in user_data["watched"]:
+        running_total += movie["rating"]
+
+    return running_total / len(user_data["watched"])
 # -----------------------------------------
 # ------------- WAVE 3 --------------------
 # -----------------------------------------
