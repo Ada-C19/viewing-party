a4ebc8e9370a6e8d32a597ba587b6a85d5bc07d2
Elena Estefania Reyes <estefyreyes.92@gmail.com>
2023-03-28 09:07:12 -0400

    create function create_movie in where if statements evaluate if the parameters are truthy and then create and return a dictionary with those parameters otherwise return None

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,8 +1,18 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    pass
-    title_string = title
+    #create and empty dictionary to store title, genre and rating of the movie
+    movie_dictionary = {}
+    #this if statement evaluate that title, genre and rating
+    #have some value different from None
+    if title and genre and rating:
+        #if the parameters are truthy then we add title,genre and rating
+        #to movie_dictionary
+        movie_dictionary["title"] = title
+        movie_dictionary["genre"] = genre
+        movie_dictionary["rating"] = rating
+        return movie_dictionary
+    return None
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
