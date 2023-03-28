96c09cf84a692c20e53be54e1f4e0812caabf5fc
Kimberly Lantigua <klantigua11@gmail.com>
2023-03-27 20:55:05 -0700

    defined create_movie() function in wave one. Passed all associated tests in the test_wave_01 file

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,6 +1,20 @@
 # ------------- WAVE 1 --------------------
-
+# 1st function in wave_01
 def create_movie(title, genre, rating):
+    movie = {}
+
+    # checks to see if all values are truthy
+    if title and genre and rating: 
+        movie["title"] = title
+        movie["genre"] = genre
+        movie["rating"] = rating
+        return movie
+
+    return None
+    
+
+# 3rd function in wave_01
+def add_to_watch_list(user_data, movie): 
     pass
 
 # -----------------------------------------
