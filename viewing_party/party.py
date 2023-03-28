0a94329a225e653c3e40d4cd71bcfa0beef34a0f
Stacy <stacydodson5@gmail.com>
2023-03-28 11:51:33 -0400

    made add_to_watchlist function

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -10,6 +10,12 @@ def create_movie(title, genre, rating):
 def add_to_watched(user_data, movie):
     user_data["watched"].append(movie)
     return user_data
+
+def add_to_watchlist(user_data, movie):
+    user_data["watchlist"].append(movie)
+    return user_data
+
+
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
