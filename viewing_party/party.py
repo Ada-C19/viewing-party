bdad5f4ef13c321cf6918e2f301fe8d02c43f99b
Virginia Ramos <virginia.rare@gmail.com>
2023-03-27 21:34:56 -0500

    New watch movie function

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -21,7 +21,27 @@ def add_to_watchlist(user_data, movie):
     watch_list = user_data
     return watch_list
 
-# def watch_movie():
+def watch_movie(user_data, title):
+    # Create a new copy of the user_data dictionary to avoid modifying the original
+    new_user_data = user_data.copy()
+    # Get the current watchlist and watched lists from the new_user_data dictionary,
+    # or create empty lists if they don't exist
+    watchlist = new_user_data.get("watchlist", [])
+    watched_list = new_user_data.get("watched", [])
+    # Check each movie in the watchlist to see if it matches the given title
+    for movie in watchlist:
+        if movie["title"] == title:
+            # If a match is found, remove the movie from the watchlist and add it to the watched list
+            watchlist.remove(movie)
+            watched_list.append(movie)
+            # Update the new_user_data dictionary with the modified watchlist and watched list
+            new_user_data["watchlist"] = watchlist
+            new_user_data["watched"] = watched_list
+            # Return the updated new_user_data dictionary
+            return new_user_data
+    # If no match is found, return the original user_data dictionary unchanged
+    return new_user_data
+
 
 
 # -----------------------------------------
