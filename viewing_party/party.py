f543e9c697f1e1b4dc3229975365ef4378120f7a
Valerie Valentin <valerie.valentine93@gmail.com>
2023-03-28 14:20:47 -0700

    Finished Wave 1

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,7 +1,30 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    pass
+    if title and genre and rating:
+        return {"title": title, "genre": genre, "rating": rating}
+    else:
+        return None
+
+    
+def add_to_watched(user_data, movie):
+    user_data["watched"].append(movie)
+    return user_data
+
+def add_to_watchlist(user_data, movie):
+    user_data["watchlist"].append(movie)
+    return user_data
+
+def watch_movie(user_data, title):
+    watched_movie = user_data['watched']
+    movie_to_watchlist = user_data['watchlist']
+
+    for item in movie_to_watchlist:
+        if title == item['title']:
+            watched_movie.append(item)
+            movie_to_watchlist.remove(item)
+    return user_data
+
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
