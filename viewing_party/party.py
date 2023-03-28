825ef9ad3fc16f51ad767c222fd597fc4b205fce
Whitney <whitneyshake@gmail.com>
2023-03-28 13:50:04 -0700

    First function of Wave 1 complete and passing tests

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,7 +1,23 @@
 # ------------- WAVE 1 --------------------
+# MOVIE_TITLE_1 = "It Came from the Stack Trace"
+# GENRE_1 = "Horror"
+# RATING_1 = 3.5
 
 def create_movie(title, genre, rating):
-    pass
+    new_movie = {}
+    if None in (title, genre, rating):
+            return None
+    else:
+        new_movie["title"] = title
+        new_movie["genre"] = genre
+        new_movie["rating"] = rating
+        return new_movie
+
+# def add_to_watched():
+
+# def add_to_watchlist():
+
+# def watch_movie():
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
