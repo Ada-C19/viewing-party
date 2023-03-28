caccc20b81c11802465fd6d8e17c8b007df1b19d
Ariel Higuera <purpl3m00ns@gmail.com>
2023-03-28 12:51:27 -0700

    finished wave 2

--- tests/test_wave_01.py
+++ tests/test_wave_01.py
@@ -140,7 +140,7 @@ def test_adds_movie_to_non_empty_user_watchlist():
     assert movie in updated_data["watchlist"]
     assert FANTASY_2 in updated_data["watchlist"]
 
-@pytest.mark.skip()
+#@pytest.mark.skip()
 def test_moves_movie_from_watchlist_to_empty_watched():
     # Arrange
     janes_data = {
@@ -159,12 +159,14 @@ def test_moves_movie_from_watchlist_to_empty_watched():
     assert len(updated_data["watchlist"]) == 0
     assert len(updated_data["watched"]) == 1
     
-    raise Exception("Test needs to be completed.")
+    # raise Exception("Test needs to be completed.")
     # *******************************************************************************************
     # ****** Add assertions here to test that the correct movie was added to "watched" **********
     # *******************************************************************************************
+    assert janes_data["watched"][0]["title"] == MOVIE_TITLE_1
+    assert janes_data["watchlist"] == []
 
-@pytest.mark.skip()
+# @pytest.mark.skip()
 def test_moves_movie_from_watchlist_to_watched():
     # Arrange
     movie_to_watch = HORROR_1
@@ -183,12 +185,14 @@ def test_moves_movie_from_watchlist_to_watched():
     assert len(updated_data["watchlist"]) == 1
     assert len(updated_data["watched"]) == 2
     
-    raise Exception("Test needs to be completed.")
+    # raise Exception("Test needs to be completed.")
     # *******************************************************************************************
     # ****** Add assertions here to test that the correct movie was added to "watched" **********
     # *******************************************************************************************
+    assert janes_data["watched"][1] == movie_to_watch
+    assert janes_data["watchlist"] == [FANTASY_1]
 
-@pytest.mark.skip()
+# @pytest.mark.skip()
 def test_does_nothing_if_movie_not_in_watchlist():
     # Arrange
     movie_to_watch = HORROR_1
--- viewing_party/party.py
+++ viewing_party/party.py
@@ -26,16 +26,56 @@ def watch_movie(user_data, title):
     '''
     Function moves a movie from user_data "watchlist" list to "watched" list
     '''
+    for i in range(len(user_data["watchlist"])):
+        if user_data["watchlist"][i]["title"] == title:
+            user_data["watched"].append(user_data["watchlist"][i])
+            user_data["watchlist"].remove(user_data["watchlist"][i])
+    return user_data
 
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
-
-
+def get_watched_avg_rating(user_data):
+    rating = 0
+    try:
+        for i in range(len(user_data["watched"])):
+            rating += user_data["watched"][i]["rating"]
+        return rating/len(user_data["watched"])   
+    except ZeroDivisionError:
+        return 0.0
+    
+def get_most_watched_genre(user_data):
+    genre_list = []
+    try:
+        for i in range(len(user_data["watched"])):
+                genre_list.append(user_data["watched"][i]["genre"])
+        
+        sorted_genre_list = sorted(genre_list,key=genre_list.count,reverse=True)
+        return sorted_genre_list[0]
+    except IndexError:
+        return None
 # -----------------------------------------
 # ------------- WAVE 3 --------------------
 # -----------------------------------------
+
+def get_unique_watched(user_data):
+    same_movies = []
+    unique_movies = []
+
+    for i in range(len(user_data["watched"])):
+        for j in range(len(user_data["friends"])):
+            for k in range(len(user_data["friends"][j]["watched"])):
+                if user_data["watched"][i]["title"] == user_data["friends"][j]["watched"][k]["title"]:
+                    same_movies.append(user_data["watched"][i])
+
+    
+    for i in range(len(user_data["watched"])):
+        if i not in same_movies:
+            unique_movies.append(user_data["watched"][i])
+
+    return unique_movies
+
 
         
 # -----------------------------------------
