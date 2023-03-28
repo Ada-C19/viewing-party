3b54d93cc1117546789aa38d21821f7f387240c1
Angie Tran <tranangiekim@gmail.com>
2023-03-27 21:22:28 -0600

    finished Wave 1

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,7 +1,28 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    pass
+    if title and genre and rating:
+        return {
+            "title": title,
+            "genre": genre,
+            "rating": rating}
+    
+def add_to_watched(user_data, movie):
+    user_data["watched"].append(movie)
+    return user_data
+
+def add_to_watchlist(user_data, movie):
+    user_data["watchlist"].append(movie)
+    return user_data
+
+def watch_movie(user_data, title):
+    for index in range(len(user_data["watchlist"])):
+        if user_data["watchlist"][index]["title"] == title:
+            add_to_watched(user_data, user_data["watchlist"][index])
+            del user_data["watchlist"][index]
+            break
+
+    return user_data
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
