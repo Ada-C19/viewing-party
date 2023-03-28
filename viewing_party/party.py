a5f8bd74022f8f093fd85c54f79ca14f6577f4eb
Limary Gonzalez <limaryg11@gmail.com>
2023-03-28 11:31:52 -0700

    Completed wave 1

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -9,16 +9,38 @@ def create_movie(title, genre, rating):
     if not title or not genre or not rating:
         return None
     
-    movie_ratings[“title”] = title
-    movie_ratings[“genre”] = genre
-    movie_ratings[“rating”] = rating
+    movie_ratings["title"] = title
+    movie_ratings["genre"] = genre
+    movie_ratings["rating"] = rating
     
     return movie_ratings
 
 
 def add_to_watched(user_data, movie):
-    user_data["watched"] = movie
 
+    watched = user_data["watched"]
+    watched.append(movie)
+
+    return user_data
+
+def add_to_watchlist(user_data, movie):
+
+    watch_list = user_data["watchlist"]
+    watch_list.append(movie)
+
+    return user_data
+
+def watch_movie(user_data, title):
+    # list of dictionaries(each dictionary has title, genre, rating)
+    watch_list = user_data["watchlist"]
+    # list of watched movies
+    watched = user_data["watched"]
+
+    for movie in watch_list:
+        if title in movie["title"]:
+            watch_list.remove(movie)
+            watched.append(movie)   
+    
     return user_data
 
 # -----------------------------------------
