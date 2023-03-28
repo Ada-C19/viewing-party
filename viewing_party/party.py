24921a8011bc1b648d61f5e6dacd9b148b80e83b
Moyo Moz <moyotornai@gmail.com>
2023-03-28 01:51:26 -0700

    wave_4 functions written and tests passed

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -60,6 +60,20 @@ def get_friends_unique_watched(user_data):
 # ------------- WAVE 4 --------------------
 # -----------------------------------------
 
+def get_available_recs(user_data):
+    user_watched_movies = {movie['title']: movie for movie in user_data['watched']}
+    friends_watched_movies = {}
+
+    for friend in user_data['friends']:
+        for movie in friend['watched']:
+            friends_watched_movies[movie['title']] = movie
+
+    unique_friends_watched_movies = set(friends_watched_movies.keys()).difference(user_watched_movies.keys())
+    available_movies = [friends_watched_movies[movie] for movie in unique_friends_watched_movies if friends_watched_movies[movie]['host'] in user_data['subscriptions']]
+    
+    return available_movies
+
+
 # -----------------------------------------
 # ------------- WAVE 5 --------------------
 # -----------------------------------------
