b9ef5053d882a575513662e7cc4f7175f89a8991
Angelica <angelica.yee57@gmail.com>
2023-03-28 19:46:29 -0500

    wave 2 checked

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,4 +1,7 @@
+
+
 # ------------- WAVE 1 --------------------
+
 
 def create_movie(title, genre, rating):
     """- add the `movie` to the `"watched"` list inside of `user_data`
@@ -43,22 +46,76 @@ def get_watched_avg_rating(user_data):
     """- Calculate the average rating of all movies in the watched list
 - The average rating of an empty watched list is `0.0`
 - return the average rating"""
-    pass
+    total = 0
+    avg_rating = 0
+    if user_data["watched"]:
+        for i in user_data["watched"]:
+            total += i["rating"]
+        avg_rating = total/len(user_data["watched"])
+        return avg_rating
+    else: 
+        return 0.0
+
 
 def get_most_watched_genre(user_data):
     """- Determine which genre is most frequently occurring in the watched list
 - return the genre that is the most frequently watched
 - If the value of "watched" is an empty list, `get_most_watched_genre` should return `None`."""
-    pass
+
+    list_genre = []
+    if user_data["watched"]:
+        for i in user_data["watched"]:
+            list_genre.append(i["genre"])
+        rpt_genre = max(set(list_genre), key=list_genre.count)
+        return rpt_genre
+    else: 
+        return None
 
 # -----------------------------------------
 # -----------------------------------------
 # ------------- WAVE 3 --------------------
+
+def helper_join_list_of_lists_and_remove_duplicates(list_of_lists):
+    plain_list = []
+    for sublist in list_of_lists:
+        for item in sublist:
+            plain_list.append(item)
+    
+    seen = set()
+    new_plain_list = []
+    for d in plain_list:
+        t = tuple(d.items())
+        if t not in seen:
+            seen.add(t)
+            new_plain_list.append(d)
+
+    return new_plain_list
+
 def get_unique_watched(user_data):
     """- Consider the movies that the user has watched, and consider the movies that their friends have watched. Determine which movies the user has watched, but none of their friends have watched.
 - Return a list of dictionaries, that represents a list of movies
 """
-    pass
+    list_of_user_data = []
+    list_of_friends_data = []
+    result = []
+    
+    for i in user_data["watched"]:
+        list_of_user_data.append(i)
+    print(type(list_of_user_data))
+    list_of_user_data_compiled = helper_join_list_of_lists_and_remove_duplicates(list_of_user_data)
+
+    #for i in user_data["friends"]:
+        #list_of_friends_data.append(i["watched"])
+    #print(list_of_friends_data)
+    #list_of_user_data_compiled = helper_join_list_of_lists_and_remove_duplicates(list_of_friends_data)
+    
+    #print(list_of_friends_data_compiled)
+    print(list_of_user_data_compiled)
+    #for i in list_of_user_data_compiled:
+        #if i not in list_of_friends_data_compiled:
+            #result.append(i) 
+    #return result
+
 
 def get_friends_unique_watched(user_data):
     """- Consider the movies that the user has watched, and consider the movies that their friends have watched. Determine which movies at least one of the user's friends have watched, but the user has not watched.
