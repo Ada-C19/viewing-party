bf8a61127aa0ef75b89ed794727910b965ec584f
Aiyana Ball <ballaiyana@gmail.com>
2023-03-29 12:35:09 -0700

    completed wave 3

--- tests/test_wave_01.py
+++ tests/test_wave_01.py
@@ -5,7 +5,7 @@ from viewing_party.party import *
 from tests.test_constants import *
 
 
-# @pytest.mark.skip()
+@pytest.mark.skip()
 def test_create_successful_movie():
     # Arrange
     movie_title = MOVIE_TITLE_1
@@ -21,7 +21,7 @@ def test_create_successful_movie():
     assert new_movie["rating"] == pytest.approx(RATING_1)
 
 
-# @pytest.mark.skip()
+@pytest.mark.skip()
 def test_create_no_title_movie():
     # Arrange
     movie_title = None
@@ -35,7 +35,7 @@ def test_create_no_title_movie():
     assert new_movie is None
 
 
-# @pytest.mark.skip()
+@pytest.mark.skip()
 def test_create_no_genre_movie():
     # Arrange
     movie_title = "Title A"
@@ -49,7 +49,7 @@ def test_create_no_genre_movie():
     assert new_movie is None
 
 
-# @pytest.mark.skip()
+@pytest.mark.skip()
 def test_create_no_rating_movie():
     # Arrange
     movie_title = "Title A"
@@ -63,7 +63,7 @@ def test_create_no_rating_movie():
     assert new_movie is None
 
 
-# @pytest.mark.skip()
+@pytest.mark.skip()
 def test_adds_movie_to_user_watched():
     # Arrange
     movie = {
@@ -85,7 +85,7 @@ def test_adds_movie_to_user_watched():
     assert updated_data["watched"][0]["rating"] == RATING_1
 
 
-# @pytest.mark.skip()
+@pytest.mark.skip()
 def test_adds_movie_to_non_empty_user_watched():
     # Arrange
     movie = {
@@ -106,7 +106,7 @@ def test_adds_movie_to_non_empty_user_watched():
     assert FANTASY_2 in updated_data["watched"]
 
 
-# @pytest.mark.skip()
+@pytest.mark.skip()
 def test_adds_movie_to_user_watchlist():
     # Arrange
     movie = {
@@ -128,7 +128,7 @@ def test_adds_movie_to_user_watchlist():
     assert updated_data["watchlist"][0]["rating"] == RATING_1
 
 
-# @pytest.mark.skip()
+@pytest.mark.skip()
 def test_adds_movie_to_non_empty_user_watchlist():
     # Arrange
     movie = {
@@ -149,7 +149,7 @@ def test_adds_movie_to_non_empty_user_watchlist():
     assert FANTASY_2 in updated_data["watchlist"]
 
 
-# @pytest.mark.skip()
+@pytest.mark.skip()
 def test_moves_movie_from_watchlist_to_empty_watched():
     # Arrange
     janes_data = {
@@ -176,7 +176,7 @@ def test_moves_movie_from_watchlist_to_empty_watched():
     # *******************************************************************************************
 
 
-# @pytest.mark.skip()
+@pytest.mark.skip()
 def test_moves_movie_from_watchlist_to_watched():
     # Arrange
     movie_to_watch = HORROR_1
@@ -201,7 +201,7 @@ def test_moves_movie_from_watchlist_to_watched():
     # ****** Add assertions here to test that the correct movie was added to "watched" **********
     # *******************************************************************************************
 
-# @pytest.mark.skip()
+@pytest.mark.skip()
 def test_does_nothing_if_movie_not_in_watchlist():
     # Arrange
     movie_to_watch = HORROR_1
--- viewing_party/party.py
+++ viewing_party/party.py
@@ -57,9 +57,54 @@ def get_most_watched_genre(user_data):
 # -----------------------------------------
 
 
+def get_unique_watched(user_data):
+    my_unique_movies = []
+    for my_movies in user_data["watched"]:
+        unique_movie = True
+        for friend in user_data["friends"]:
+            for friends_movies in friend["watched"]:
+                if my_movies["title"] == friends_movies["title"]:
+                    unique_movie = False
+        if unique_movie is True:
+            my_unique_movies.append(my_movies)
+    return my_unique_movies
+
+
+def get_friends_unique_watched(user_data):
+    friends_unique_movies = []
+    for friend in user_data["friends"]:
+        for friends_movies in friend["watched"]:
+            unique_movie = True
+            for my_movies in user_data["watched"]:
+                if friends_movies["title"] == my_movies["title"]:
+                    unique_movie = False
+            if unique_movie is True:
+                movie_already_in_list = False
+                for movie in friends_unique_movies:
+                    if friends_movies["title"] == movie["title"]:
+                        movie_already_in_list = True
+                if movie_already_in_list is False:
+                    friends_unique_movies.append(friends_movies)
+    return friends_unique_movies
+
+
 # -----------------------------------------
 # ------------- WAVE 4 --------------------
 # -----------------------------------------
+
+
+def get_available_recs(user_data):
+    recommended_movies = []
+    for friend in user_data["friends"]: #iterate through friends
+        for friend_movie in friend["watched"]: #iterate through friend movies
+            is_recommended = True #flag saying movie is recommended or not
+            #if the movie is in user_data["watched"]:, recommended = False
+            #if the movie's "host" is not in user_data["subscriptions"] recommended = false
+            # after passing these tests, if it is still recommended, append it to the list of recommended movie dictionaries
+    return recommended_movies
+
+
+
 
 # -----------------------------------------
 # ------------- WAVE 5 --------------------
