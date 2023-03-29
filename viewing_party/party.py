a801c2543457c97dbfe99d8da4947f85a967d697
Kimberly Lantigua <klantigua11@gmail.com>
2023-03-29 15:53:25 -0700

    Defined both functions in wave 05, completed test in asscoiated test 5 and passed all tests :)

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -130,5 +130,26 @@ def get_available_recs(user_data):
 # -----------------------------------------
 # ------------- WAVE 5 --------------------
 # -----------------------------------------
+# 1st function in wave 05 
+def get_new_rec_by_genre(user_data):
+    
+    movie_recs = []
+    genre = get_most_watched_genre(user_data)
+    friends_unique_movies = get_friends_unique_watched(user_data)
 
+    for movie in friends_unique_movies: 
+        if movie["genre"] == genre:     
+            movie_recs.append(movie)
+    
+    return movie_recs
 
+# 2nd function in wave 05
+def get_rec_from_favorites(user_data): 
+    user_unique_movies = get_unique_watched(user_data)
+    movie_recs = []
+
+    for movie in user_data["favorites"]: 
+        if movie in user_unique_movies: 
+            movie_recs.append(movie)
+    
+    return movie_recs
