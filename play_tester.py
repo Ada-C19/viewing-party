5004bb5c5a545b17cea6548ce997e208927bc150
maggie weir <mlweir98@gmail.com>
2023-03-28 20:36:21 -0400

    played around with function in playtester

--- play_tester.py
+++ play_tester.py
@@ -9,16 +9,40 @@ import pprint
 pp = pprint.PrettyPrinter(indent=4)
 
 # play testing section
-print("\n-----Wave 01 test data-----")
-pp.pprint(HORROR_1)
-pp.pprint(FANTASY_1)
-pp.pprint(FANTASY_2)
+# print("\n-----Wave 01 test data-----")
+# pp.pprint(HORROR_1)
+# pp.pprint(FANTASY_1)
+# pp.pprint(FANTASY_2)
 
 # print("\n-----Wave 02 user_data-----")
 # pp.pprint(clean_wave_2_data())
 
-#print("\n-----Wave 03 user_data-----")
-#pp.pprint(clean_wave_3_data())
+# print("\n-----Wave 03 user_data-----")
+# pp.pprint(clean_wave_3_data())
+user_data = clean_wave_3_data()
+
+def get_friends_unique_watched(user_data):
+    friends_unique_watched = []
+    friends_watched_set = set()
+    user_watched_set = set()
+
+    for friend in user_data["friends"]:
+        for movie in friend["watched"]:
+            friends_watched_set.add(movie["title"])
+
+    for i in range(len(user_data["watched"])):
+        user_watched_set.add(user_data["watched"][i]["title"])
+
+    differences = friends_watched_set.difference(user_watched_set)
+
+    for friend in user_data["friends"]:
+        for movie in friend["watched"]:
+            if movie["title"] in differences and movie not in friends_unique_watched:
+                friends_unique_watched.append(movie)
+                continue
+    print("unique friends", friends_unique_watched)
+    return friends_unique_watched
+get_friends_unique_watched(user_data)
 
 # Wave 04 user data
 #print("\n-----Wave 04 user_data-----")
