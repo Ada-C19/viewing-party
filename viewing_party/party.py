5b99182f38a808ac25422a272f7b2011c85d4311
Diana <diana38974@gmail.com>
2023-03-28 22:59:52 -0400

    implementation of wave 1

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -8,22 +8,30 @@ def create_movie(title, genre, rating):
         movie.update({"title" : title, "genre" : genre, "rating" : rating})
         return movie
 
+
 def add_to_watched(user_data, movie):
-    # user_data = {
-    #     "watched" : [movie dictionaries]
-    # }
-    
-    # movie = {
-    #     "title": "Title A",
-    #     "genre": "Horror",
-    #     "rating": 3.5
-    #     }
+
     user_data["watched"].append(movie)
     return user_data
+
 
 def add_to_watchlist(user_data, movie):
     user_data["watchlist"].append(movie)
     return user_data
+
+
+def watch_movie(user_data, title):
+    print(user_data)
+    watchlist_list_of_dict = user_data.get("watchlist")
+    print(watchlist_list_of_dict)
+
+    for watchlistDict in watchlist_list_of_dict:
+        if watchlistDict.get("title") == title:
+            watchlist_list_of_dict.remove(watchlistDict)
+            user_data["watched"].append(watchlistDict)
+            print(user_data)
+    return user_data
+
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
