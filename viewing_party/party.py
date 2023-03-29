c7e924f0884cc02ded9cb6b26fbf5f5cf4eaa266
Valerie Valentin <valerie.valentine93@gmail.com>
2023-03-28 18:01:00 -0700

    Finished wave 2

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,5 +1,8 @@
-# ------------- WAVE 1 --------------------
 
+import collections 
+
+
+# ------------- WAVE 1 --------------------
 def create_movie(title, genre, rating):
     if title and genre and rating:
         return {"title": title, "genre": genre, "rating": rating}
@@ -29,13 +32,35 @@ def watch_movie(user_data, title):
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
+def get_watched_avg_rating(user_data):
+    total_ratings = 0
+    number_movies = len(user_data["watched"])
 
+    if len(user_data["watched"]) == 0:
+        return 0.0
+    
+    for i in range(len(user_data["watched"])):
+            total_ratings += user_data["watched"][i]["rating"]
+            average_rating = total_ratings / number_movies
+    return average_rating
+
+
+
+def get_most_watched_genre(user_data):
+    genre_options = []
+
+    if user_data["watched"] == []:
+        return None    
+    
+    for i in range(len(user_data["watched"])):
+        genre_options.append(user_data["watched"][i]["genre"])
+        highest_watched = (max(set(genre_options), key=genre_options.count))
+    return highest_watched
 
 # -----------------------------------------
 # ------------- WAVE 3 --------------------
 # -----------------------------------------
-
-        
+    
 # -----------------------------------------
 # ------------- WAVE 4 --------------------
 # -----------------------------------------
