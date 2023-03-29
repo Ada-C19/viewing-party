580f1c0303e5442408b9655c2c7cd9f503b5e446
louloera <louloera@gmail.com>
2023-03-29 15:11:47 -0700

    wrong commit

--- tests/test_wave_03.py
+++ tests/test_wave_03.py
@@ -2,7 +2,7 @@ import pytest
 from viewing_party.party import *
 from tests.test_constants import *
 
-@pytest.mark.skip()
+#@pytest.mark.skip()
 def test_my_unique_movies():
     # Arrange
     amandas_data = clean_wave_3_data()
@@ -16,7 +16,7 @@ def test_my_unique_movies():
     assert INTRIGUE_2 in amandas_unique_movies
     assert amandas_data == clean_wave_3_data()
 
-@pytest.mark.skip()
+#@pytest.mark.skip()
 def test_my_not_unique_movies():
     # Arrange
     amandas_data = clean_wave_3_data()
@@ -42,6 +42,7 @@ def test_friends_unique_movies():
     assert HORROR_1 in friends_unique_movies
     assert FANTASY_4 in friends_unique_movies
     assert amandas_data == clean_wave_3_data()
+
 
 @pytest.mark.skip()
 def test_friends_unique_movies_not_duplicated():
