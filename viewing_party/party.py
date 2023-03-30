435c1d9c5221cbe343110c5844316b0f337e8845
Edith <mxedithbaltazar@gmail.com>
2023-03-30 13:51:15 -0700

    Last wave all but last function done

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -227,3 +227,27 @@ def get_new_rec_by_genre(user_data):
 
 
     
+# def get_rec_from_favorites(user_data):
+#     '''
+#     take one parameter: user_data
+
+#     Consider the user's most frequently watched genre. Then, determine a list of 
+#     '''
+# user_frequently_watched_genre = get_most_watched_genre(user_data)
+# '''
+# recommended movies. A movie should be added to this list if and only if:
+# The user has not watched it
+# At least one of the user's friends has watched
+# The "genre" of the movie is the same as the user's most frequent genre
+# Return the list of recommended movies
+# Create a function named get_rec_from_favorites. This function should...
+# take one parameter: user_data
+# user_data will have a field "favorites". The value of "favorites" is a list of 
+# movie dictionaries
+# This represents the user's favorite movies
+# Determine a list of recommended movies. A movie should be added to this list 
+# if and only if:
+# The movie is in the user's "favorites"
+# None of the user's friends have watched it
+# Return the list of recommended movies
+# '''
