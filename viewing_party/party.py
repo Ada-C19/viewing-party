a7facf979acb39eba331c77001548f61436d010d
jasmine <jtmsoto@gmail.com>
2023-03-30 09:28:28 -0700

    Refactor get_most_watched_genre to use max function

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -58,7 +58,6 @@ def get_watched_avg_rating(user_data):
 def get_most_watched_genre(user_data):
     user_genres = {}
     most_watched = None
-    times_watched = 0
 
     # Iterates through each movie in watched list and adds
     # each genre to a user_genres dictionary with an initial value of 1.
@@ -69,11 +68,8 @@ def get_most_watched_genre(user_data):
         else:
             user_genres[movie["genre"]] += 1
 
-    # Iterates through the genres in user_genres to find most watched
-    for genre in user_genres:
-        if user_genres[genre] > times_watched:
-            most_watched = genre
-            times_watched = user_genres[genre]
+    if user_genres:
+        most_watched = max(user_genres, key=user_genres.get)
 
     return most_watched
 
