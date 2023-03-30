33af5bc23d8a92abb162bb417dd68fac14ad0abc
Diana <diana38974@gmail.com>
2023-03-30 19:16:40 -0400

    Wave 03 implementation

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -40,9 +40,48 @@ def watch_movie(user_data, title):
 
 # -----------------------------------------
 # ------------- WAVE 3 --------------------
-# -----------------------------------------
 
-        
+
+def get_unique_watched(user_data):
+
+    user_unique_movies = []  # a list of dictionaries
+    movies_titles_friends_watched = []  # a list of strings
+
+    for item in user_data.get("friends"):
+        for x in item.get("watched"):
+            movies_titles_friends_watched.append(x.get("title"))
+    
+    for item in user_data.get("watched"):
+        movie_user_has_watched = item
+        if movie_user_has_watched.get("title") not in movies_titles_friends_watched:
+                user_unique_movies.append(movie_user_has_watched)
+
+    return user_unique_movies
+
+
+def get_friends_unique_watched(user_data):
+
+    friends_movies = [] # a list of dictionaries
+    movies_titles_user_watched = [] # a list of strings
+
+    for item in user_data.get("watched"):
+        movies_titles_user_watched.append(item.get("title"))
+
+    for item in user_data.get("friends"):
+        for x in item.get("watched"):
+            if x.get("title") not in movies_titles_user_watched:
+                friends_movies.append(x)
+
+    new_list = []
+    for dictionary in friends_movies:
+        if dictionary not in new_list:
+            new_list.append(dictionary)
+    
+    return new_list
+
+    
+    
+
 # -----------------------------------------
 # ------------- WAVE 4 --------------------
 # -----------------------------------------
