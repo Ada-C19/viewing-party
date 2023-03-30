2bffca7da36ea81a3b51019b02d12b2e271ff7c5
Zhuo <zhuo.parse@gmail.com>
2023-03-30 12:41:23 -0400

    Modified wave 2

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,30 +1,40 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
+
     if title is None or genre is None or rating is None:
         return None
+    
     new_movie = {}
     new_movie["title"] = title
     new_movie["genre"] = genre
     new_movie["rating"] = rating
+
     return new_movie
 
 def add_to_watched(user_data, movie):
+
     if movie:
        user_data["watched"].append(movie)
+
     return user_data
 
 def add_to_watchlist (user_data, movie):
+
     if movie:
         user_data["watchlist"].append(movie)
+
     return user_data
 
 def watch_movie(user_data, title):
+
     watch_list = user_data.get("watchlist")
+
     for movie in watch_list:
         if title == movie.get("title"):
             watch_list.remove(movie)
             user_data["watched"].append(movie)
+
     return user_data
 
 
@@ -33,35 +43,38 @@ def watch_movie(user_data, title):
 # -----------------------------------------
 
 def get_watched_avg_rating(user_data):
+
     avg_rating = 0.0
     watched_list = user_data.get("watched")
-    sum = 0.0
+    rating_sum = 0.0
+
     for movie in watched_list:
         current_rating = movie["rating"]
-        sum += current_rating
+        rating_sum += current_rating
+
     if len(watched_list) == 0:
-        return 0.0
-    avg_rating = sum / len(watched_list)
+        return avg_rating
+    
+    avg_rating = rating_sum / len(watched_list)
 
     return avg_rating
 
 def get_most_watched_genre(user_data):
+
     watched_list = user_data.get("watched")
-    most_watched_count = 0
-    most_watched = None
     genre_dict = {}
+    
+    if not watched_list:
+        return None
     
     for movie in watched_list:
         current_genre = movie["genre"]
-        if current_genre in genre_dict.keys():
-            genre_dict[current_genre] += 1
-        else:
-            genre_dict[current_genre]= 1
-    for genre, frequency in genre_dict.items():
-        if most_watched_count < frequency:
-            most_watched_count = frequency
-            most_watched = genre
-    return most_watched
+        genre_dict[current_genre] = genre_dict.get(current_genre, 0) + 1
+      
+    if not genre_dict:
+        return None
+    
+    return max(genre_dict, key=genre_dict.get)
 
 
 # -----------------------------------------
@@ -69,6 +82,7 @@ def get_most_watched_genre(user_data):
 # -----------------------------------------
     
 def get_friend_watched_list(user_data):
+
     friend_list = user_data.get("friends")
     friend_watched_list = []
     
@@ -92,6 +106,7 @@ def get_unique_watched(user_data):
                 unique_movie.append(movie)
             
     return unique_movie
+
 
 def get_friends_unique_watched(user_data):
 
@@ -126,7 +141,6 @@ def get_available_recs(user_data):
     return recommended_movie_list
 
 
-
 # -----------------------------------------
 # ------------- WAVE 5 --------------------
 # -----------------------------------------
@@ -138,7 +152,7 @@ def get_new_rec_by_genre (user_data):
     user_watched_most_list = get_most_watched_genre(user_data)
     only_friend_watched_list = get_friends_unique_watched(user_data)
     
-    if only_friend_watched_list is None or user_watched_most_list is None:
+    if not only_friend_watched_list or not user_watched_most_list:
         return recommended_movie
     
     for movie in only_friend_watched_list:
@@ -150,14 +164,16 @@ def get_new_rec_by_genre (user_data):
 
 
 def get_rec_from_favorites (user_data):
+
     recommended_movie = []
     user_favorite_movie = user_data["favorites"]
     user_unique_watched_movie = get_unique_watched(user_data)
 
-    if user_favorite_movie is None or user_unique_watched_movie is None:
+    if not user_favorite_movie or not user_unique_watched_movie:
         return recommended_movie
 
     for movie in user_unique_watched_movie:
         if movie in user_favorite_movie:
             recommended_movie.append(movie)
+
     return recommended_movie
