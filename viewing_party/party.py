341ba60c1e840dd33f64bcddb815daf3a45844a0
Muniba <mbashirkimes@gmail.com>
2023-03-29 20:13:40 -0400

    add functions to wave 3

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -67,6 +67,58 @@ def get_most_watched_genre(user_data):
 # -----------------------------------------
 # ------------- WAVE 3 --------------------
 # -----------------------------------------
+    
+def get_friend_watched_list(user_data):
+    friend_list = user_data.get("friends")
+    friend_watched_list = []
+    
+
+    for friend in friend_list:
+        for movie in friend["watched"]:
+            friend_watched_list.append(movie)
+        
+    return friend_watched_list
+
+
+def get_unique_watched(user_data): 
+
+    watched_list = user_data.get("watched")
+    friend_watched_list = get_friend_watched_list(user_data)
+
+    unique_movie = []
+
+    for movie in watched_list:
+        if movie not in friend_watched_list:
+                unique_movie.append(movie)
+            
+    return unique_movie
+
+def get_friends_unique_watched(user_data):
+
+    watched_list = user_data.get("watched")
+    friend_watched_list = get_friend_watched_list(user_data)
+    friends_unique_watched_list = []
+    
+    no_dup_friends_watched_list = [i for n, i in enumerate(friend_watched_list)
+        if i not in friend_watched_list[n + 1:]]
+
+    for movie in no_dup_friends_watched_list:
+        if movie not in watched_list:
+                friends_unique_watched_list.append(movie)
+    
+    return friends_unique_watched_list
+
+
+
+
+
+
+
+
+
+
+
+
 
         
 # -----------------------------------------
