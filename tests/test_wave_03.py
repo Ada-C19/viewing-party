6b4002389104c4d12b52258ab59ecb38a4f7338b
Katherine Guarnizo <guarnizo.katherine.5@gmail.com>
2023-03-30 11:26:48 -0400

    update assert test in wave 3 and begin wave 4

--- tests/test_wave_03.py
+++ tests/test_wave_03.py
@@ -54,7 +54,7 @@ def test_friends_unique_movies_not_duplicated():
 
     # Assert
     assert len(friends_unique_movies) == 3
-    assert "Zero Dark Python" == friends_unique_movies[1]["title"]
+    assert INTRIGUE_3 in friends_unique_movies
 
 # @pytest.mark.skip()
 def test_friends_not_unique_movies():
--- viewing_party/party.py
+++ viewing_party/party.py
@@ -105,6 +105,20 @@ def get_friends_unique_watched(user_data):
 # ------------- WAVE 4 --------------------
 # -----------------------------------------
 
+def get_available_recs(user_data):
+    friend_unique = get_friends_unique_watched(user_data)
+    user_subscription_list = user_data["subscriptions"]
+
+    for friend in user_data["friends"]:
+        for friend_movie in friend["watched"]:
+            for host in friend_movie["host"]:
+                
+
+
+    
+
+
+
 # -----------------------------------------
 # ------------- WAVE 5 --------------------
 # -----------------------------------------
