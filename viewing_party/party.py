25bfdfecaea3adf7750aedab73b9342075d58af5
Kathleen <kathleen.n.shin@gmail.com>
2023-03-30 13:27:51 -0700

    Completed get_most_watched_genre function

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -8,22 +8,19 @@ def create_movie(title, genre, rating):
 # The values of these key-value pairs should be appropriate values
 # If title is falsy, genre is falsy, or rating is falsy, this function should return None
 
-# create empty dictionary
     movie_dict = {}
-# if any arguments are falsy, return None
+
     if not title:
         return None
     elif not genre:
         return None
     elif not rating:
         return None
-# else: add arguments to empty dictionary with key value pairs
     else: 
         movie_dict["title"] = title
         movie_dict["genre"] = genre
         movie_dict["rating"] = rating
 
-# return the dictionary
     return movie_dict
 
 create_movie("Title", "Genre", "Rating")
@@ -34,8 +31,6 @@ def add_to_watched(user_data, movie):
 
     user_data["watched"].append(movie)
     return user_data
-# #  we need to add movie to the list that is the value dictionary user_data 
-#     return (user_data)
 
 add_to_watched({"watched": []}, {
         "title": "MOVIE_TITLE_1",
@@ -62,9 +57,6 @@ def add_to_watchlist(user_data, movie):
 
 #---Wave_1_function_4---
 def watch_movie(user_data, title):
-
-    # if title not in user_data["watchlist"]["title"]:
-    #     return user_data
     for movie in user_data["watchlist"]:
         if title != (movie["title"]):
             return user_data
@@ -102,11 +94,6 @@ title = "iron man"
 
 
 def get_watched_avg_rating(user_data):
-# - navigate to janes_data rating (watchlist -> dictionaries, rating)
-# - new list of ratings
-# - find function to calculate average get_watched_avg_rating
-# - return 0.0 for empty list
-
 
     ratings = []
     
@@ -122,8 +109,6 @@ def get_watched_avg_rating(user_data):
     return ratings_average
 
 
-        
-
 # ---Wave_2_function_2---
 def get_most_watched_genre(user_data):
 
@@ -133,51 +118,56 @@ def get_most_watched_genre(user_data):
         return None
     
     for movie in user_data["watched"]:
+
         
-        # movie_genre_dict[(movie["genre"])].update(movie)
         title = movie["title"]
         genre = movie["genre"]
+        
         if genre not in movie_genre_dict:
             movie_genre_dict[genre] = []
+        
         movie_genre_dict[genre].append(title)
-   
-        # append new value to existing key 
-    print(movie_genre_dict)
 
-    # figure out max count
     max_genre = None
     max_count = 0
 
-    
+    for genre in movie_genre_dict:
+        genre_length = len(movie_genre_dict[genre])
+        if genre_length >= max_count:
+            max_genre = genre
+            max_count = len(movie_genre_dict.values())
 
-    return max(movie_genre_dict)
-
-# movie_genre_dict.update({(movie["genre"]): movie})
-
-
-print (get_most_watched_genre(janes_data))
-
-
-
+    return max_genre
 
 # ------------- WAVE 3 --------------------
 
 #---Wave_3_function_1---
-# def get_unique_watched(user_data):
+def get_unique_watched(user_data):
+    pass
 
 #---Wave_3_function_2---
-# def get_friends_unique_watched(user_data):
+def get_friends_unique_watched(user_data):
+    pass
 
 # ------------- WAVE 4 --------------------
 #---Wave_4_function_1---
-# def get_available_recs(user_data):
-
+def get_available_recs(user_data):
+    pass
 
 # ------------- WAVE 5 --------------------
 # ---Wave_5_function_1---
-# def get_new_rec_by_genre(user_data):
+def get_new_rec_by_genre(user_data):
+    most_watched_genre = 
+    get_most_watched_genre(user_data)
 
 # ---Wave_5_function_2---
-# def get_rec_from_favorites(user_data):
+def get_rec_from_favorites(user_data):
+    pass
 
 
+# to do:
+# update tests for exception handling
+# update wave 2 function 2
+# finish wave 5
+# add/edit comments?
+# refactor?
