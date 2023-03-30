e4cf718d6fe6c988e75b02c1faa626440ab771a0
Anna Lima <annavanwesep@gmail.com>
2023-03-29 18:32:57 -0700

    added assert statements to wave03 tests, functions still passing

--- tests/test_wave_03.py
+++ tests/test_wave_03.py
@@ -43,7 +43,7 @@ def test_friends_unique_movies():
     assert FANTASY_4 in friends_unique_movies
     assert amandas_data == clean_wave_3_data()
 
-@pytest.mark.skip()
+# @pytest.mark.skip()
 def test_friends_unique_movies_not_duplicated():
     # Arrange
     amandas_data = clean_wave_3_data()
@@ -55,10 +55,12 @@ def test_friends_unique_movies_not_duplicated():
     # Assert
     assert len(friends_unique_movies) == 3
 
-    raise Exception("Test needs to be completed.")
-    # *************************************************************************************************
-    # ****** Add assertions here to test that the correct movies are in friends_unique_movies **********
-    # **************************************************************************************************
+    # raise Exception("Test needs to be completed.")
+    # *********************************************************************
+    # ****** Complete the Assert Portions of these tests **********
+    # *********************************************************************
+    assert FANTASY_4 in friends_unique_movies
+    assert INTRIGUE_3 in friends_unique_movies
 
 # @pytest.mark.skip()
 def test_friends_not_unique_movies():
