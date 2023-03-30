ff248439152d5afcb658c9abffe599e97aa3732b
Sophia Tran <sophiat8832@gmail.com>
2023-03-30 13:54:06 -0700

    refactored functions in Wave 1

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -3,31 +3,29 @@
 
 def create_movie(title, genre, rating):
     new_movie = {}
+
     if not title or not genre or not rating:
         return None
-
     else:
         new_movie['title'] = title
         new_movie['genre'] = genre
         new_movie['rating'] = rating
+
     return new_movie
 
 def add_to_watched(user_data, movie):
-    for data in user_data:
-        user_data["watched"].append(movie)
-    
+    user_data["watched"].append(movie)
     return user_data
 
 def add_to_watchlist(user_data, movie):
-    for data in user_data:
-        user_data["watchlist"].append(movie)
+    user_data["watchlist"].append(movie)
     return user_data
 
 def watch_movie(user_data, title):
-    for user in user_data["watchlist"]:
-        if user["title"] == title:
-            user_data["watchlist"].remove(user)
-            user_data["watched"].append(user)
+    for movie in user_data["watchlist"]:
+        if movie["title"] == title:
+            user_data["watchlist"].remove(movie)
+            user_data["watched"].append(movie)
 
     return user_data
 # -----------------------------------------
