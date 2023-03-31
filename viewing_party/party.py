ee3a628e1f4539f2177e7b4d72fcfbeba51d2e14
Valerie Valentin <valerie.valentine93@gmail.com>
2023-03-30 17:27:09 -0700

    Finished Wave 5

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,3 +1,88 @@
+import copy
+
+MOVIE_TITLE_1 = "It Came from the Stack Trace"
+GENRE_1 = "Horror"
+RATING_1 = 3.5
+
+HORROR_1 = {
+    "title": MOVIE_TITLE_1,
+    "genre": GENRE_1,
+    "rating": RATING_1
+}
+FANTASY_1 = {
+    "title": "The Lord of the Functions: The Fellowship of the Function",
+    "genre": "Fantasy",
+    "rating": 4.8
+}
+FANTASY_2 = {
+    "title": "The Lord of the Functions: The Two Parameters",
+    "genre": "Fantasy",
+    "rating": 4.0
+}
+FANTASY_3 = {
+    "title": "The Lord of the Functions: The Return of the Value",
+    "genre": "Fantasy",
+    "rating": 4.0
+}
+FANTASY_4 = {
+    "title": "The Programmer: An Unexpected Stack Trace",
+    "genre": "Fantasy",
+    "rating": 4.0
+}
+ACTION_1 = {
+    "title": "The JavaScript and the React",
+    "genre": "Action",
+    "rating": 2.2
+}
+ACTION_2 = {
+    "title": "2 JavaScript 2 React",
+    "genre": "Action",
+    "rating": 4.2
+}
+ACTION_3 = {
+    "title": "JavaScript 3: VS Code Lint",
+    "genre": "Action",
+    "rating": 3.5
+}
+INTRIGUE_1 = {
+    "title": "Recursion",
+    "genre": "Intrigue",
+    "rating": 2.0
+}
+INTRIGUE_2 = {
+    "title": "Instructor Student TA Manager",
+    "genre": "Intrigue",
+    "rating": 4.5
+}
+INTRIGUE_3 = {
+    "title": "Zero Dark Python",
+    "genre": "Intrigue",
+    "rating": 3.0
+}
+
+HORROR_1b = copy.deepcopy(HORROR_1)
+FANTASY_1b = copy.deepcopy(FANTASY_1)
+FANTASY_2b = copy.deepcopy(FANTASY_2)
+FANTASY_3b = copy.deepcopy(FANTASY_3)
+FANTASY_4b = copy.deepcopy(FANTASY_4)
+ACTION_1b = copy.deepcopy(ACTION_1)
+ACTION_2b = copy.deepcopy(ACTION_2)
+ACTION_3b = copy.deepcopy(ACTION_3)
+INTRIGUE_1b = copy.deepcopy(INTRIGUE_1)
+INTRIGUE_2b = copy.deepcopy(INTRIGUE_2)
+INTRIGUE_3b = copy.deepcopy(INTRIGUE_3)
+
+HORROR_1b["host"] = "netflix"
+FANTASY_1b["host"] = "netflix"
+FANTASY_2b["host"] = "netflix"
+FANTASY_3b["host"] = "amazon"
+FANTASY_4b["host"] = "hulu"
+ACTION_1b["host"] = "amazon"
+ACTION_2b["host"] = "amazon"
+ACTION_3b["host"] = "hulu"
+INTRIGUE_1b["host"] = "hulu"
+INTRIGUE_2b["host"] = "disney+"
+INTRIGUE_3b["host"] = "disney+"
 
 # ------------- WAVE 1 --------------------
 
@@ -95,7 +180,40 @@ def get_friends_unique_watched(user_data):
 # -----------------------------------------
 
 def get_available_recs(user_data):
+    # user_data = {
+    #     "watched": [
+    #         FANTASY_1b, 
+    #         FANTASY_2b, 
+    #         FANTASY_3b, 
+    #         ACTION_1b, 
+    #         INTRIGUE_1b, 
+    #         INTRIGUE_2b
+    #         ],  
+    #     "friends":  [
+    #         {
+    #             "watched": [
+    #                 FANTASY_1b,
+    #                 FANTASY_3b,
+    #                 FANTASY_4b,
+    #                 HORROR_1b,
+    #             ]
+    #         },
+    #         {
+    #             "watched": [
+    #                 FANTASY_1b,
+    #                 FANTASY_4b,
+    #                 ACTION_1b,
+    #                 INTRIGUE_1b,
+    #                 INTRIGUE_3b,
+    #             ]
+    #         }  
+    #     ]
+    # }
+
+    user_data["subscriptions"] = ["netflix", "hulu"] 
+
     available_recs = []
+
     friends_movies_recs = get_friends_unique_watched(user_data)
 
     for i in range(len(friends_movies_recs)):
@@ -103,48 +221,11 @@ def get_available_recs(user_data):
             available_recs.append(friends_movies_recs[i])
     return available_recs
         
+        
 
 # -----------------------------------------
 # ------------- WAVE 5 --------------------
 # -----------------------------------------
 
 def get_new_rec_by_genre(user_data):
-    recommended_genre = []
-    fave_genre = get_most_watched_genre(user_data)
-    unique_list_of_movies_watched_by_friends = get_most_watched_genre(user_data)
-    user_titles_that_match_my_fave_genre = []
-
-    for movie in user_data["watched"]:
-        if movie["genre"] == fave_genre:
-            user_titles_that_match_my_fave_genre.append(movie["title"])
-    
-    for indivd_movie in unique_list_of_movies_watched_by_friends:
-        for individ_movie in friend[0]["watched"]:
-            if individ_movie["genre"] == fave_genre and movie["title"] not in user_titles_that_match_my_fave_genre:
-                recommended_genre.append(individ_movie)
-    return recommended_genre
-
-
-
-
-
-    
-    # recommended_genre = []
-    # new_list = []
-    # fave_genre = get_most_watched_genre(user_data)
-   
-    # for movie in user_data["friends"]:
-    #     for i in movie["watched"]:
-    #         new_list.append(i)
-    # for movie in new_list:
-    #     if movie["genre"] == fave_genre:
-    #         recommended_genre.append(movie)
-    # return recommended_genre
-
-    
-    # for friend in unique_list_of_movies_watched_by_friends:
-    #     for indiv_movie in friend["watched"]:
-    #         if indiv_movie["genre"] == fave_genre and movie["title"] not in user_title_that_match_my_fave_genre:
-    #             recommended_genre.append(indiv_movie)
-    # return recommended_genre       
-    
+    pass
