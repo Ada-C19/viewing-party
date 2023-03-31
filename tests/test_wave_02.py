e808eecae28f193a8f8281a2b20bd990a0e8546c
Diana <diana38974@gmail.com>
2023-03-30 22:13:00 -0400

    Wave 04 implementation

--- tests/test_wave_02.py
+++ tests/test_wave_02.py
@@ -3,7 +3,7 @@ from viewing_party.party import *
 from tests.test_constants import *
 
 # @pytest.mark.skip()
-# def test_calculates_watched_average_rating():
+def test_calculates_watched_average_rating():
     # Arrange
     janes_data = clean_wave_2_data()
 
