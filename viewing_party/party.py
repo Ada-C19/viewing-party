817d3398900f50d6061395eaa67179bbfc3b323a
Zhuo <zhuo.parse@gmail.com>
2023-03-31 12:11:35 -0400

    Modified wave 5 for better time complexity

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -86,9 +86,11 @@ def get_friend_watched_list(user_data):
     friend_list = user_data.get("friends")
     friend_watched_list = []
     
+
     for friend in friend_list:
         for movie in friend["watched"]:
             friend_watched_list.append(movie)
+        
     return friend_watched_list
 
 
@@ -96,11 +98,13 @@ def get_unique_watched(user_data):
 
     watched_list = user_data.get("watched")
     friend_watched_list = get_friend_watched_list(user_data)
+
     unique_movie = []
-    
+
     for movie in watched_list:
         if movie not in friend_watched_list:
                 unique_movie.append(movie)
+            
     return unique_movie
 
 
@@ -109,18 +113,14 @@ def get_friends_unique_watched(user_data):
     watched_list = user_data.get("watched")
     friend_watched_list = get_friend_watched_list(user_data)
     friends_unique_watched_list = []
-
-    seen = set()
-    no_dup_friends_watched_list = []
-    for dictionary in friend_watched_list:
-        t = tuple(dictionary.items())
-        if t not in seen:
-            seen.add(t)
-            no_dup_friends_watched_list.append(dictionary)
+    
+    no_dup_friends_watched_list = [i for n, i in enumerate(friend_watched_list)
+        if i not in friend_watched_list[n + 1:]]
 
     for movie in no_dup_friends_watched_list:
         if movie not in watched_list:
                 friends_unique_watched_list.append(movie)
+    
     return friends_unique_watched_list
 
         
@@ -137,6 +137,7 @@ def get_available_recs(user_data):
     for movie in friends_watched_list:
         if movie["host"] in subscriptions_list:
             recommended_movie_list.append(movie)
+
     return recommended_movie_list
 
 
@@ -148,16 +149,16 @@ def get_available_recs(user_data):
 def get_new_rec_by_genre (user_data):
 
     recommended_movie = []
-    user_watched_most_list = get_most_watched_genre(user_data)
+    user_watched_most_genre = get_most_watched_genre(user_data)
     only_friend_watched_list = get_friends_unique_watched(user_data)
     
-    if not only_friend_watched_list or not user_watched_most_list:
+    if not only_friend_watched_list or not user_watched_most_genre:
         return recommended_movie
     
     for movie in only_friend_watched_list:
-        if movie["genre"] in user_watched_most_list:
+        if movie["genre"]  == user_watched_most_genre:
             recommended_movie.append(movie)
- 
+    
     return recommended_movie
 
 
@@ -166,13 +167,14 @@ def get_rec_from_favorites (user_data):
 
     recommended_movie = []
     user_favorite_movie = user_data["favorites"]
+    user_favorite_movie_set = {tuple(movie.values()) for movie in user_favorite_movie}
     user_unique_watched_movie = get_unique_watched(user_data)
 
     if not user_favorite_movie or not user_unique_watched_movie:
         return recommended_movie
 
     for movie in user_unique_watched_movie:
-        if movie in user_favorite_movie:
+        if tuple(movie.values()) in user_favorite_movie_set:
             recommended_movie.append(movie)
 
     return recommended_movie
