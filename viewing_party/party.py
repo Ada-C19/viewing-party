1dd27186f5115336d85f007137e27c81af693236
Monica <monicalagdaan@hotmail.com>
2023-05-22 15:28:21 -0700

    finished all of wave 1, passing 9/11 tests for wave 1

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,7 +1,27 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    pass
+    if title and genre and rating:
+        movie_dict = { 
+            "title": title,
+            "genre": genre,
+            "rating": rating
+        }
+        return movie_dict
+
+def add_to_watched(user_data, movie):
+    user_data["watched"].append(movie)
+    return user_data
+
+def add_to_watchlist(user_data, movie):
+    user_data["watchlist"].append(movie)
+    return user_data
+
+def watch_movie(user_data, title):
+    if title in user_data["watchlist"]:
+        user_data["watchlist"].remove(title)
+        user_data["watched"].append(title)
+    return user_data
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
